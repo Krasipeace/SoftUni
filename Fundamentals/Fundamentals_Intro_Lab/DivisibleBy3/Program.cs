e04f9c24a5e33994d0bfeed80ef22d7df6dd3499
Krasipeace <66023495+Krasipeace@users.noreply.github.com>
2022-05-26 02:42:25 +0300

    tasks

---
+++ Fundamentals/Fundamentals_Intro_Lab/DivisibleBy3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DivisibleBy3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
