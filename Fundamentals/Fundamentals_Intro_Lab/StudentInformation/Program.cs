ea6f07f2618ae17e4c36bf89d8f8b0595ff4ed49
Krasipeace <66023495+Krasipeace@users.noreply.github.com>
2022-05-26 02:44:31 +0300

    tasks

---
+++ Fundamentals/Fundamentals_Intro_Lab/StudentInformation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StudentInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            Console.Write($"Name: {studentName}, Age: {age}, Grade: {grade:f2}");
+        }
+    }
+}
