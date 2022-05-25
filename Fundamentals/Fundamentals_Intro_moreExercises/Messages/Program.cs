deb1bfeba512805edcd49aa95cf2695956c95e52
Krasipeace <66023495+Krasipeace@users.noreply.github.com>
2022-05-26 02:35:12 +0300

    fundamentals tasks
    loops, conditional statements

---
+++ Fundamentals/Fundamentals_Intro_moreExercises/Messages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            for (int i = 1; i <= input; i++)
+            {
+                string number = Console.ReadLine();
+
+                int lengthNumber = number.Length;
+                //Console.WriteLine(lengthNumber);
+                int digit = number[0] - '0';
+                int offset = (digit - 2) * 3;
+
+                if (digit == 0)
+                {
+                    output += (char)(digit + 32);
+                    continue;
+                }
+                if (digit == 8 || digit == 9)
+                {
+                    offset++;
+                }
+                int letter = offset + lengthNumber - 1;
+                output += (char)(letter + 97);
+            }
+            Console.WriteLine(output);
+        }       
+    }
+}
---
+++ Fundamentals/Fundamentals_Intro_moreExercises/ReverseString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var reverse = input.ToCharArray();
+            Array.Reverse(reverse);
+            Console.WriteLine(reverse);
+        }
+    }
+}
