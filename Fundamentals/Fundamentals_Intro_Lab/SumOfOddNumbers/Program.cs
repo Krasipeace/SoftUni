90227ef6500f3be0a3354ff6ef5e0a9c8d8cfd5b
Krasipeace <66023495+Krasipeace@users.noreply.github.com>
2022-05-26 03:07:57 +0300

    lab tasks

---
+++ Fundamentals/Fundamentals_Intro_Lab/SumOfOddNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int number = 1; number <= limit; number++)
+            {
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine(number);
+                    sum += number;
+                }
+               else
+               {
+                   limit++;
+               }                
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
