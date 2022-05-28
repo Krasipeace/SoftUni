7ac85c96e739e73fb042db5b8834abd9f0390b46
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-29 00:06:08 +0300

    Update Program.cs

--- Fundamentals/3_1_F_Arrays_Lab/01.DayOfWeek/Program.cs
+++ Fundamentals/3_1_F_Arrays_Lab/01.DayOfWeek/Program.cs
@@ -6,7 +6,21 @@ namespace _01.DayOfWeek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dayOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1: Console.WriteLine(dayOfWeek[0]); break;
+                case 2: Console.WriteLine(dayOfWeek[1]); break;
+                case 3: Console.WriteLine(dayOfWeek[2]); break;
+                case 4: Console.WriteLine(dayOfWeek[3]); break;
+                case 5: Console.WriteLine(dayOfWeek[4]); break;
+                case 6: Console.WriteLine(dayOfWeek[5]); break;
+                case 7: Console.WriteLine(dayOfWeek[6]); break;
+                default: Console.WriteLine("Invalid day!"); break;
+            } 
+            
         }
     }
 }
