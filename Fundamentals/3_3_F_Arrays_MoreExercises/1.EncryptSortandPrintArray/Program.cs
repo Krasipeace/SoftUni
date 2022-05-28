ed338c07d4d1075c4fd625c952ee05ff4932f95f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-28 20:05:49 +0300

    add folders for arrays exercises

---
+++ Fundamentals/3_3_F_Arrays_MoreExercises/1.EncryptSortandPrintArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.EncryptSortandPrintArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
