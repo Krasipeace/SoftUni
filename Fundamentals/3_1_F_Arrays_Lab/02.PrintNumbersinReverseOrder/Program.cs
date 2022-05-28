6d7b4a5d6e191f1cab563762eee5d3c5d0cc6007
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-29 00:18:23 +0300

    exercises

---
+++ Fundamentals/3_1_F_Arrays_Lab/02.PrintNumbersinReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.PrintNumbersinReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumbers = int.Parse(Console.ReadLine());
+            int[] numbersArray = new int[inputNumbers];
+
+            for (int i = 0; i < inputNumbers; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbersArray[i] = number;
+            }
+            for (int i = numbersArray.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbersArray[i]} ");
+            }
+        }
+    }
+}
