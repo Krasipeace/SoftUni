455440b4521a8d77f3c35b9ff6ed6bb12e57b528
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-29 16:37:53 +0300

    update condenseArraytoNumber

--- Fundamentals/3_1_F_Arrays_Lab/08. Condense Array to Number/Program.cs
+++ Fundamentals/3_1_F_Arrays_Lab/08. Condense Array to Number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08._Condense_Array_to_Number
 {
@@ -6,7 +7,32 @@ namespace _08._Condense_Array_to_Number
     {
         static void Main(string[] args)
         {
-            
+            int[] inputArray = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int length = inputArray.Length - 1;
+            int[] condensedArray = new int[length];
+
+            while (length > 0)
+            {
+
+                for (int i = 0; i < length; i++)
+                {
+                    condensedArray[i] = inputArray[i] + inputArray[i + 1];
+                    //inputArray[i] = condensedArray[i];                           //remove comment, and add comment on second loop (but makes program slow)...
+                }
+
+                for (int i = 0; i < length; i++)
+                {
+                    inputArray[i] = condensedArray[i];
+                }
+
+                length--;
+            }
+
+            Console.WriteLine(inputArray[0]);
         }
     }
 }
