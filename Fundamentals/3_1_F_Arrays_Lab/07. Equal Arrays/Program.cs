eda9201ca405f3df86914fa3f28046112a038919
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-29 13:26:04 +0300

    solved 07.EqualArrays

---
+++ Fundamentals/3_1_F_Arrays_Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            int result = 0;
+            bool isIdentical = false;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {              
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isIdentical = false;
+                    break;                    
+                }
+                else if (secondArray[i] == firstArray[i])
+                {
+                    sum = firstArray[i] + secondArray[i];
+                    result += sum / 2;
+                    isIdentical = true;
+                }
+            }
+            if (isIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {result}");
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
