52d6462f95b3fd631cc5226ed6e80393c9d334b2
Krasimir Dramaliev <krasipeace@abv.bg>
2022-05-31 00:13:04 +0300

    ch6 bookbg exercises

---
+++ Other Exercises/Book_Fundamentals of Computer Programming with Csharp BG - Exercises/FundamentalsCsharpBookCh_6/zad10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zad10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());          
+            for (int i = 1; i <= n; i++)
+            {               
+
+                for (int j = i; j <= n + 1; j++)
+                {
+                    Console.Write($"{j} ");                    
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Other Exercises/Book_Fundamentals of Computer Programming with Csharp BG - Exercises/FundamentalsCsharpBookCh_6/zad6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace DivideOfFactoriels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Combination of task 6 and task 7
+
+            //Програма, която пресмята N!/K! за дадени N и K (1<K<N).
+
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("K = ");
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                n = n * i;
+            }
+            for (int i = k - 1; i > 0; i--)
+            {
+                k = k * i;
+            }
+            Console.WriteLine($"N! = {n} K! = {k}");
+            int ndivk = n / k;
+            Console.WriteLine($"N!/K! = {ndivk}");
+            //Програма, която пресмята N!*K!/(N-K)! за дадени N и K (1<K<N).
+            BigInteger nk = n - k; 
+            for (BigInteger j = nk - 1; j > 0; j--)
+            {
+                nk = nk * j;
+            }
+            BigInteger nkResult = (n * k) / (nk);
+            Console.WriteLine($"N! * K! / (N - K)! = {nkResult}");
+
+
+        }
+    }
+}
