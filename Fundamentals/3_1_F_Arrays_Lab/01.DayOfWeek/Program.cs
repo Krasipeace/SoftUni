3730c93823d01ff0503e9c65ed64c4a2fc877b9b
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-01 19:44:57 +0300

    add ver.2 of task 1

--- Fundamentals/3_1_F_Arrays_Lab/01.DayOfWeek/Program.cs
+++ Fundamentals/3_1_F_Arrays_Lab/01.DayOfWeek/Program.cs
@@ -9,18 +9,28 @@ namespace _01.DayOfWeek
             string[] dayOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
             int day = int.Parse(Console.ReadLine());
 
-            switch (day)
-            {
-                case 1: Console.WriteLine(dayOfWeek[0]); break;
-                case 2: Console.WriteLine(dayOfWeek[1]); break;
-                case 3: Console.WriteLine(dayOfWeek[2]); break;
-                case 4: Console.WriteLine(dayOfWeek[3]); break;
-                case 5: Console.WriteLine(dayOfWeek[4]); break;
-                case 6: Console.WriteLine(dayOfWeek[5]); break;
-                case 7: Console.WriteLine(dayOfWeek[6]); break;
-                default: Console.WriteLine("Invalid day!"); break;
-            } 
+            //version 1:
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine(dayOfWeek[0]); break;
+            //    case 2: Console.WriteLine(dayOfWeek[1]); break;
+            //    case 3: Console.WriteLine(dayOfWeek[2]); break;
+            //    case 4: Console.WriteLine(dayOfWeek[3]); break;
+            //    case 5: Console.WriteLine(dayOfWeek[4]); break;
+            //    case 6: Console.WriteLine(dayOfWeek[5]); break;
+            //    case 7: Console.WriteLine(dayOfWeek[6]); break;
+            //    default: Console.WriteLine("Invalid day!"); break;
+            //} 
             
+            //version 2:
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine(dayOfWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
         }
     }
 }
