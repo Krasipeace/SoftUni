a828db90e25f241d5c802d636e30ded213476aef
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-05 18:58:36 +0300

    more exercises

--- Fundamentals/3_3_F_Arrays_MoreExercises/4.FoldAndSum/Program.cs
+++ Fundamentals/3_3_F_Arrays_MoreExercises/4.FoldAndSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4.FoldAndSum
 {
@@ -6,7 +7,21 @@ namespace _4.FoldAndSum
     {
         static void Main(string[] args)
         {
-            
+            int[] inputArray = Console.ReadLine()
+                                      .Split()
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            int fold = inputArray.Length / 4;
+            int[] sumArray = new int[2 * fold];
+
+            for (int i = 0; i < fold; i++)
+            {
+                sumArray[i] = inputArray[fold - (i + 1)] + inputArray[fold + i];
+                sumArray[sumArray.Length - 1 - i] = inputArray[sumArray.Length - 1 - i + fold] + 
+                                                    inputArray[sumArray.Length - 1 - i + fold + 2 * i + 1];
+            }
+            Console.WriteLine(string.Join(" ", sumArray));
         }
     }
 }
