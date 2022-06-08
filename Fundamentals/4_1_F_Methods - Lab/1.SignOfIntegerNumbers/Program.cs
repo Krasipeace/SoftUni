c690c0c68ef366eb3c3bf7494d3b450d36099be1
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-08 15:27:45 +0300

    methods lab

---
+++ Fundamentals/4_1_F_Methods - Lab/1.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSign(int.Parse(Console.ReadLine()));
+        }
+
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
