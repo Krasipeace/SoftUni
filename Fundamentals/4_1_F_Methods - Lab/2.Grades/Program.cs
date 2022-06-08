95391048faf6dbfafea5eeec16395e4e87c79870
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-08 15:55:55 +0300

    methods lab

---
+++ Fundamentals/4_1_F_Methods - Lab/2.Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _2.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintInWords(double.Parse(Console.ReadLine()));
+        }
+
+        private static void PrintInWords(double grade)
+        {
+            string gradeInWords = string.Empty;
+
+            if (grade <= 2.99)
+            {
+                gradeInWords = "Fail";
+            }
+            else if (grade <= 3.49)
+            {
+                gradeInWords = "Poor";
+            }
+            else if (grade <= 4.49)
+            {
+                gradeInWords = "Good";
+            }
+            else if (grade <= 5.49)
+            {
+                gradeInWords = "Very good";
+            }
+            else if (grade <= 6.00)
+            {
+                gradeInWords = "Excellent";
+            }
+            Console.WriteLine(gradeInWords);
+        }
+    }
+}
