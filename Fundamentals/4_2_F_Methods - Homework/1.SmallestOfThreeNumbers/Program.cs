c88f1bbebbe35044dfb985ff3b5b558bc7bbac50
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-09 16:56:25 +0300

    methods

---
+++ Fundamentals/4_2_F_Methods - Homework/1.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Fundamentals/4_3_F_Methods - More_Exercises/1.DataTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
