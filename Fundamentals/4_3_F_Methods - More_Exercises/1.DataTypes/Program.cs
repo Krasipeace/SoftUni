0ec04154575c7a7f0ac707b2a655b1649a515989
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-09 19:20:43 +0300

    Update Program.cs

--- Fundamentals/4_3_F_Methods - More_Exercises/1.DataTypes/Program.cs
+++ Fundamentals/4_3_F_Methods - More_Exercises/1.DataTypes/Program.cs
@@ -6,7 +6,45 @@ namespace _1.DataTypes
     {
         static void Main(string[] args)
         {
-            
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "int":
+                    int inputInt = int.Parse(Console.ReadLine());
+                    Console.WriteLine(GetType(inputInt));
+                    break;
+                case "real":
+                    double inputReal = double.Parse(Console.ReadLine());
+                    Console.WriteLine(GetType($"{inputReal:f2}"));
+                    break;
+                case "string":
+                    string inputString = Console.ReadLine();
+                    Console.WriteLine(GetType($"{inputString}"));
+                    break;
+            }
+
+        }
+
+        static int GetType(int inputInt)
+        {
+            int result = 0;
+            result = inputInt * 2;
+            return result;
+        }
+
+        static double GetType(double inputReal)
+        {
+            double result = 0.0;
+            result = (double)inputReal * 1.5;
+            return result;
+        }
+
+        private static string GetType(string inputString) //hm...
+        {
+            string result = string.Empty;
+            result = "$" + inputString + "$";
+            return result;
         }
     }
 }
