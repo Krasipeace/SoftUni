2ce26aa2e312a508450e45535da8aaf49f825997
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-15 11:25:12 +0300

    methods - exercise

--- Fundamentals/4_2_F_Methods - Homework/10.TopNumber/Program.cs
+++ Fundamentals/4_2_F_Methods - Homework/10.TopNumber/Program.cs
@@ -6,7 +6,45 @@ namespace _10.TopNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int range = int.Parse(Console.ReadLine());
+
+            for (int checkNumber = 1; checkNumber <= range; checkNumber++)
+            {
+                if (isSumDivisionByEight (checkNumber) && isThereOddDigit (checkNumber))
+                {
+                    Console.WriteLine(checkNumber);
+                }
+            }
+        }
+
+        private static bool isThereOddDigit(int topNumber)
+        {
+            while (topNumber > 0)
+            {
+                if (topNumber % 10 % 2 != 0)
+                {
+                    return true;
+                }
+                topNumber /= 10;
+            }
+            return false;
+        }
+
+        private static bool isSumDivisionByEight(int topNumber)
+        {
+            int sumDigits = 0;
+
+            while (topNumber > 0)
+            {
+                sumDigits += topNumber % 10;
+                topNumber /= 10;
+            }
+
+            if (sumDigits % 8 == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- Fundamentals/4_2_F_Methods - Homework/11.ArrayManipulator/Program.cs
+++ Fundamentals/4_2_F_Methods - Homework/11.ArrayManipulator/Program.cs
@@ -24,26 +24,128 @@ namespace _11.ArrayManipulator
                 }
                 else
                 {
-                    FindNumbers(array, command[0], command[1]);
+                    FindNumbers(array, command[0], int.Parse(command[1]), command[2]);
                 }
 
                 command = Console.ReadLine().Split();
             }
+            Console.WriteLine($"[{string.Join(", ", array)}]");
         }
 
         private static int[] ExchangeArray(int[] array, int givenIndex)
         {
+            if (givenIndex >= array.Length || givenIndex < 0)
+            {
+                Console.WriteLine("Invalid index");
+                return array;
+            }
+
+            int[] exchangeArray = new int[array.Length];
+            int index = 0;
             
+            for (int i = givenIndex + 1; i < array.Length; i++)
+            {
+                exchangeArray[index] = array[i];
+                index++;
+            }
+            for (int i = 0; i <= givenIndex; i++)
+            {
+                exchangeArray[index] = array[i];
+                index++;
+            }
+            return exchangeArray;
         }
 
-        private static void FindMinOrMax(int[] array, string v1, string v2)
+        private static void FindMinOrMax(int[] array, string minOrMax, string evenOrOdd)
         {
-            
+            int index = -1;
+            int minIndexNumber = int.MaxValue;
+            int maxIndexNumber = int.MinValue;
+            int resultEvenOrOdd = 1;
+
+            if (evenOrOdd == "even")
+            {
+                resultEvenOrOdd = 0;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == resultEvenOrOdd)
+                {
+                    if (minOrMax == "min" && minIndexNumber >= array[i])
+                    {
+                        minIndexNumber = array[i];
+                        index = i;
+                    }
+                    else if (minOrMax == "max" && maxIndexNumber <= array[i])
+                    {
+                        maxIndexNumber = array[i];
+                        index = i;
+                    }
+                }
+            }
+
+            Console.WriteLine(index > -1 ? index.ToString() : "No matches");
+
         }
 
-        private static void FindNumbers(int[] array, string v1, string v2)
+        private static void FindNumbers(int[] array, string position, int countNumbers, string evenOrOdd)
         {
-            
+            if (countNumbers > array.Length)
+            {
+                Console.WriteLine("Invalid count");
+                return;
+            }
+
+            if (countNumbers == 0)
+            {
+                Console.WriteLine($"[]");
+                return;
+            }
+
+            int resultEvenOrOdd = 1;
+            if (evenOrOdd == "even")
+            {
+                resultEvenOrOdd = 0;
+            }
+            int counter = 0;
+
+            List<int> numbers = new List<int>();
+
+            if (position == "first")
+            {
+                foreach (int number in array)
+                {
+                    if (number % 2 == resultEvenOrOdd)
+                    {
+                        counter++;
+                        numbers.Add(number);
+                    }
+
+                    if (counter == countNumbers)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = array.Length - 1; i >= 0; i--)
+                {
+                    if (array[i] % 2 == resultEvenOrOdd)
+                    {
+                        counter++;
+                        numbers.Add(array[i]);
+                    }
+
+                    if (counter == countNumbers)
+                    {
+                        break;
+                    }
+                }
+                numbers.Reverse();
+            }
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
         }
     }
 }
