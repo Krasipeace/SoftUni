9c3601ebb2bd9cec0a81cce940c2b9c7da92d063
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-21 19:51:29 +0300

    exercises

---
+++ Fundamentals/0_2_F_MidExam Exercises/05 July 2020/1. SoftUni Reception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int teacherOne = int.Parse(Console.ReadLine());
+            int teacherTwo = int.Parse(Console.ReadLine());
+            int teacherThree = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            int counter = 0;            
+            int answersPerHour = teacherOne + teacherTwo + teacherThree;
+                     
+            while (students > 0)
+            {
+                students -= answersPerHour;
+                counter++;
+                
+                if (counter % 4 ==0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Time needed: {counter}h.");
+        }
+    }
+}
---
+++ Fundamentals/0_2_F_MidExam Exercises/05 July 2020/2. Array Modifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Array_Modifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
