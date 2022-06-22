b33a107bf59900224a8d562fb588a4e2bdaec751
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-22 16:44:04 +0300

    Update Program.cs

--- Fundamentals/0_2_F_MidExam Exercises/05 July 2020/3. Numbers/Program.cs
+++ Fundamentals/0_2_F_MidExam Exercises/05 July 2020/3. Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Numbers
 {
@@ -6,7 +8,30 @@ namespace _3._Numbers
     {
         static void Main(string[] args)
         {
+            List<double> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
 
+            double averageNumber = Math.Floor(numbers.Sum() / numbers.Count);
+            if (numbers.Count < 5)
+            {
+                Console.WriteLine("No");
+
+                return;
+            }
+
+            List<double> topNumbers = new List<double>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > averageNumber)
+                {
+                    topNumbers.Add(numbers[i]);
+                }
+            }
+            topNumbers.Sort();
+            topNumbers.Reverse();
+            var topFive = topNumbers.Take(5);
+
+            Console.WriteLine(string.Join(" ", topFive));
         }
     }
 }
