6de1d9acfe1e3e7953c8fa443780b712e272e6fd
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-24 00:21:53 +0300

    Update Program.cs

--- Fundamentals/0_2_F_MidExam Exercises/29 02 2020/2. Shopping List/Program.cs
+++ Fundamentals/0_2_F_MidExam Exercises/29 02 2020/2. Shopping List/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Shopping_List
 {
@@ -6,6 +8,75 @@ namespace _2._Shopping_List
     {
         static void Main(string[] args)
         {
+            List<string> list = Console.ReadLine().Split("!", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Go Shopping!")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split();
+                string action = tokens[0];
+
+                switch (action)
+                {
+                    case "Urgent":
+                        Urgent(tokens[1], list);                         //add item at the start of the list
+                        break;
+                    case "Unnecessary":
+                        Unnecessary(tokens[1], list);                    //remove item
+                        break;
+                    case "Correct":
+                        Correct(tokens[1], tokens[2], list);             //change name of item
+                        break;
+                    case "Rearrange":
+                        Rearrange(tokens[1], list);                      //remove item from current position and add it to the end of the list
+                        break;
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", list));
+        }
+
+        private static void Rearrange(string item, List<string> list)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                list.Add(item);
+            }
+                                 
+        }
+
+        private static void Correct(string oldItemName, string newItemName, List<string> list)
+        {
+            if (list.Contains(oldItemName))
+            {
+                int positionOfItem = list.IndexOf(oldItemName);
+                list.Insert(positionOfItem, newItemName);
+                list.Remove(oldItemName);
+            }
+           
+        }
+
+        private static void Unnecessary(string item, List<string> list)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+            }
+            
+        }
+
+        private static void Urgent(string item, List<string> list)
+        {
+            if (!list.Contains(item))
+            {
+                list.Insert(0, item);
+            }
             
         }
     }
