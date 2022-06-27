7b9b61fef4f7bdf133b4fbd532534d3ea6a8e57f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-06-27 15:37:13 +0300

    objectsAndClasses

---
+++ Fundamentals/5_1_F_Objects and Classes/01. Randomize Words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputList = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Random randomList = new Random();
+
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                int randomWord = randomList.Next(inputList.Count);
+                string a = inputList[randomWord];
+                string b = inputList[i];
+
+                inputList[randomWord] = b;
+                inputList[i] = a;
+            }
+            Console.WriteLine(String.Join(Environment.NewLine, inputList));
+        }
+    }
+}
