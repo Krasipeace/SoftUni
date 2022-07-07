c2090c6a0bc7d88be4a4c399e201c602d7c493b4
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-07 17:21:49 +0300

    АА - exercises

---
+++ Fundamentals/7_2_F_AssociativeArrays/1. Count Chars in a String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> word = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ' ')
+                {
+                    continue;
+                }
+                char letter = input[i];
+
+                if (!word.ContainsKey(letter))
+                {
+                    word[letter] = 0;
+                }
+                word[letter] += 1;
+            }
+
+            foreach (var chr in word)
+            {
+                Console.WriteLine($"{chr.Key} -> {chr.Value}");
+            }
+        }
+    }
+}
---
+++ Fundamentals/7_2_F_AssociativeArrays/2. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            while(true)
+            {
+                string resource = Console.ReadLine();
+                if (resource.Equals("stop"))
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+                if (!dictionary.ContainsKey(resource))
+                {
+                    dictionary.Add(resource, 0);
+                }
+
+                dictionary[resource] += quantity;
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+
+        }
+    }
+}
