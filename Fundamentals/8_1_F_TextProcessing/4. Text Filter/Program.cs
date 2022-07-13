d5f04866dc2692922ba898b9d1848646032744bf
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-13 22:26:23 +0300

    tp

---
+++ Fundamentals/8_1_F_TextProcessing/4. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] censoredWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+
+            foreach (var censoredWord in censoredWords)
+            {
+                string censoringSymbol = new string('*', censoredWord.Length);
+
+                text = text.Replace(censoredWord, censoringSymbol);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Fundamentals/8_1_F_TextProcessing/5. DigitsLettersAndOther/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] digits = input
+                .Where(ch => char.IsDigit(ch))
+                .ToArray();
+
+            char[] letters = input
+                .Where(ch => char.IsLetter(ch))
+                .ToArray();
+
+            char[] others = input
+                .Where(ch => !char.IsLetterOrDigit(ch))
+                .ToArray();
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
