93fc369f5b868007410986d6e4cf216d47bee3ea
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-17 22:41:09 +0300

    exercises

--- Fundamentals/8_3_F_TextProcessing - More Exercises/3. Treasure Finder/Program.cs
+++ Fundamentals/8_3_F_TextProcessing - More Exercises/3. Treasure Finder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace _3._Treasure_Finder
 {
@@ -6,7 +8,43 @@ namespace _3._Treasure_Finder
     {
         static void Main(string[] args)
         {
-            
+            int[] keys = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string text = Console.ReadLine();
+
+            string resource = string.Empty;
+            string coordinates = string.Empty;
+
+            while (true)
+            {
+                if (text == "find")
+                {
+                    break;
+                }
+
+                int maxLength = Math.Max(keys.Length, text.Length);
+                StringBuilder newText = new StringBuilder(maxLength);
+
+                for (int i = 0; i < maxLength; i++)
+                {
+                    newText.Append((char)(text[i] - keys[i % keys.Length]));
+                }
+
+                int startIndexResource = newText.ToString().IndexOf('&') + 1;
+                int lastIndexResource = newText.ToString().IndexOf('&', startIndexResource + 1);
+                int resourceLength = lastIndexResource - startIndexResource;
+
+                resource = newText.ToString().Substring(startIndexResource, resourceLength);
+
+                int startIndexCoordinates = newText.ToString().IndexOf('<') + 1;
+                int lastIndexCoordinates = newText.ToString().IndexOf('>');
+                int coordinatesLength = lastIndexCoordinates - startIndexCoordinates;
+
+                coordinates = newText.ToString().Substring(startIndexCoordinates, coordinatesLength);
+
+                Console.WriteLine($"Found {resource} at {coordinates}");
+
+                text = Console.ReadLine();
+            }
         }
     }
 }
---
+++ Fundamentals/8_3_F_TextProcessing - More Exercises/5. HTML/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+            string comment = Console.ReadLine();
+            List<string> comments = new List<string>();
+
+            while (true)
+            {
+
+                if (comment == "end of comments")
+                {
+                    break;
+                }
+
+                comments.Add(comment);
+
+                comment = Console.ReadLine();
+            }
+
+            PrintResult(title, content, comments);
+        }
+
+        private static void PrintResult(string title, string content, List<string> comments)
+        {
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {content}");
+            Console.WriteLine("</article>");
+
+            foreach (var eachComment in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"    {eachComment}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
