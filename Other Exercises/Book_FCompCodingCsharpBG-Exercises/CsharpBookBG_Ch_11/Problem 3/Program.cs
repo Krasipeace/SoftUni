0b4951a45ce15b51f44fb096839f06d41456cf05
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-21 20:38:24 +0300

    classes

---
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; //which day is today
+
+namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Today is: ");
+
+            DateTime dateTime = DateTime.Today;
+
+            Console.Write(dateTime.DayOfWeek);
+        }
+    }
+}
---
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 7/Examples/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem_7.Examples
+{
+    internal class Class1
+    {
+
+    }
+}
