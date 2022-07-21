e742140252c4699532bfaf84fd736aebfbc23b7c
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-22 00:10:37 +0300

    classes

--- Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 7/Examples/Class1.cs
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 7/Examples/Class1.cs
@@ -1,11 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Problem_7;
 
 namespace Problem_7.Examples
 {
-    internal class Class1
+    public class Cat
     {
+        // Field name
+        private string name;
+
+        // Field color
+        private string color;
+
+        public string Name
+        {
+            // Getter of the property "Name"
+            get
+            {
+                return this.name;
+            }
+            // Setter of the property "Name"
+            set
+
+            {
+                this.name = value;
+            }
+        }
+
+        public string Color
+        {
+            // Getter of the property "Color"
+            get
+            {
+                return this.color;
+            }
+
+            // Setter of the property "Color"
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        // Default constructor
+        public Cat()
+        {
+            this.name = "Unnamed";
+
+            this.color = "gray";
+        }
+
+        // Constructor with parameters
+        public Cat(string name, string color)
+        {
+            this.name = name;
+
+            this.color = color;
+        }
+
+        // Method SayMiau
+        public void SayMiau()
+        {
+            Console.WriteLine("Cat {0} said: MEOWWWW!", name);
+        }
+
+    }
+
+    public class Sequence
+    {
+        // Static field, holding the current sequence value
+        private static int currentValue = 0;
+        // Intentionally deny instantiation of this class
+        private Sequence()
+        {
+
+        }
+
+        // Static method for taking the next sequence value
+        public static int NextValue()
+        {
+            currentValue++;
+
+            return currentValue;
+        }
 
     }
 }
--- Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 7/Program.cs
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_11/Problem 7/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Problem_7.Examples;
+using Problem_7.Examples; //problem 7 and problem 8 combined
 
 namespace Problem_7
 {
@@ -7,91 +7,23 @@ namespace Problem_7
     {
         static void Main(string[] args)
         {
+          //  Console.WriteLine("Sequence[1..3]: {0}, {1}, {2}", Sequence.NextValue(), Sequence.NextValue(), Sequence.NextValue());
+          //
+          //  Cat someCat = new Cat();
+          //  someCat.SayMiau();
+          //  Console.WriteLine("The color of cat {0} is {1}.", someCat.Name, someCat.Color);
+          //
+          //  someCat = new Cat("Johnny", "brown");
+          //  someCat.SayMiau();
+          //  Console.WriteLine("The color of cat {0} is {1}.", someCat.Name, someCat.Color);
 
-        }
-    }
-
-
-
-    public class Cat
-    {
-        // Field name
-        private string name;
-
-        // Field color
-        private string color;
-
-        public string Name
-        {
-            // Getter of the property "Name"
-            get
+            string name = "";
+            for (int i = 0; i < 10; i++)
             {
-                return this.name;
+                Cat cat = new Cat(name + Sequence.NextValue(), "Black");
+                cat.SayMiau();
             }
-            // Setter of the property "Name"
-            set
-
-            {
-                this.name = value;
-            }
+            
         }
-
-        public string Color
-        {
-            // Getter of the property "Color"
-            get
-            {
-                return this.color;
-            }
-
-            // Setter of the property "Color"
-            set
-            {
-                this.color = value;
-            }
-        }
-
-        // Default constructor
-        public Cat()
-        {
-            this.name = "Unnamed";
-
-            this.color = "gray";
-        }
-
-        // Constructor with parameters
-        public Cat(string name, string color)
-        {
-            this.name = name;
-
-            this.color = color;
-        }
-
-        // Method SayMiau
-        public void SayMiau()
-        {
-            Console.WriteLine("Cat {0} said: MEOWWWW!", name);
-        }
-
-    }
-
-    public class Sequence
-    {
-        // Static field, holding the current sequence value
-        private static int currentValue = 0;
-        // Intentionally deny instantiation of this class
-        private Sequence()
-        {
-
-        }
-
-        // Static method for taking the next sequence value
-        public static int NextValue()
-        {
-            currentValue++;
-
-            return currentValue;
-        }
-
     }
 }
