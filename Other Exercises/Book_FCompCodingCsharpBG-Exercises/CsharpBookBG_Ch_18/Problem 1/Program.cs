806467b5cd148a5d62eca5bb420c1d780f04a7e4
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-27 18:09:14 +0300

    dictionaries

---
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_18/Problem 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; //count numbers in List
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers(divider-interval): ");
+
+            List<int> list = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var repeatedNumbers = list.GroupBy(x => x)
+                                            .Select(num => new {Value = num.Key, Count = num.Count()})
+                                            .OrderBy(x => x.Count);
+
+            foreach (var item in repeatedNumbers)
+            {
+                Console.WriteLine($"{item.Value} -> {item.Count}");
+            }
+        }
+    }
+}
---
+++ Other Exercises/Book_FCompCodingCsharpBG-Exercises/CsharpBookBG_Ch_18/Problem 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; //Count words in text
+using System.Collections.Generic;
+
+namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+
+            string[] text = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> newList = new Dictionary<string, int>();
+
+            foreach (var word in text)
+            {
+                if (newList.ContainsKey(word))
+                {
+                    newList[word]++;
+                }
+                else
+                {
+                    newList.Add(word, 1);
+                }
+            }
+
+            foreach (var item in newList)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
