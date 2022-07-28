7ed91909a43a1a2d34eb6b541c979e7d0041b456
Krasimir Dramaliev <krasipeace@abv.bg>
2022-07-28 16:36:25 +0300

    exam prep exercises

---
+++ Fundamentals/0_4_F_FinalExam Exercises/1. The Imitation Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _1._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string commands = Console.ReadLine();
+
+            while (commands != "Decode")
+            {
+                string[] tokens = commands.Split("|", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Move":
+                        input = Move(input, tokens);
+                        break;
+                    case "Insert":
+                        input = Insert(input, tokens);
+                        break;
+                    case "ChangeAll":
+                        input = ChangeAll(input, tokens);
+                        break;
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {input}");
+        }
+
+        private static string Move(string input, string[] tokens)
+        {
+            int index = int.Parse(tokens[1]);
+            input = input.Remove(0, index) + input.Substring(0, index);
+
+            return input;
+        }
+
+        private static string Insert(string input, string[] tokens)
+        {
+            int index = int.Parse(tokens[1]);
+            string value = tokens[2];
+            input = input.Insert(index, value);
+
+            return input;
+        }
+
+        private static string ChangeAll(string input, string[] tokens)
+        {
+            string substring = tokens[1];
+            string replacement = tokens[2];
+            input = input.Replace(substring, replacement);
+
+            return input;
+        }
+    }
+}
---
+++ Fundamentals/0_4_F_FinalExam Exercises/2. Ad Astra/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2._Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([?<=#|\|])(?<foodType>[A-Za-z ]+)\1(?<foodDate>[\d]{2}\/[\d]{2}\/[\d]{2})\1(?<nutrition>[\d]+)\1";
+
+            string input = Console.ReadLine();
+            const int DAILY_CALORIES = 2000;
+            int allFoodNutrition = 0;
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            StringBuilder output = new StringBuilder();
+
+            foreach (Match item in matches)
+            {
+                string foodType = item.Groups["foodType"].Value;
+                string foodDate = item.Groups["foodDate"].Value;
+                int foodNutrition = int.Parse(item.Groups["nutrition"].Value);
+                
+                allFoodNutrition += foodNutrition;
+
+                output.AppendLine($"Item: {foodType}, Best before: {foodDate}, Nutrition: {foodNutrition}");
+            }
+
+            if (allFoodNutrition < DAILY_CALORIES)
+            {
+                Console.WriteLine("You have food to last you for: 0 days!");
+            }
+            else
+            {
+                Console.WriteLine($"You have food to last you for: {allFoodNutrition / DAILY_CALORIES} days!");
+                Console.WriteLine(output.ToString());
+            }
+        }
+    }
+}
