e4cbc85820fbff843f5d188c555bb3c0893967c6
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-03 17:41:15 +0300

    examPrep

---
+++ Fundamentals/0_4_F_FinalExam Exercises/3. Heroes of Code and Logic VII Objects/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Heroes_of_Code_and_Logic_VII_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hero> heroes = new List<Hero>();
+
+            int addNewHero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < addNewHero; i++)
+            {
+                string[] heroData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string heroName = heroData[0];
+                int heroHealth = int.Parse(heroData[1]);
+                int heroMana = int.Parse(heroData[2]);
+
+                Hero hero = new Hero()
+                {
+                    Name = heroName,
+                    Health = heroHealth,
+                    Mana = heroMana
+                };
+                heroes.Add(hero);
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] action = command.Split(" - ");
+                switch (action[0])
+                {
+                    case "CastSpell":
+                        CastSpell(action[1], int.Parse(action[2]), action[3], heroes);
+                        break;
+                    case "TakeDamage":
+                        TakeDamage(action[1], int.Parse(action[2]), action[3], heroes);
+                        break;
+                    case "Recharge":
+                        Recharge(action[1], int.Parse(action[2]), heroes);
+                        break;
+                    case "Heal":
+                        Heal(action[1], int.Parse(action[2]), heroes);
+                        break;
+                }
+            }
+
+            foreach (Hero aliveHero in heroes)
+            {
+                Console.WriteLine($"{aliveHero.Name}");
+                Console.WriteLine($"  HP: {aliveHero.Health}");
+                Console.WriteLine($"  MP: {aliveHero.Mana}");
+            }
+        }
+
+        private static void CastSpell(string heroName, int spellMana, string spellName, List<Hero> heroes)
+        {
+            Hero hero = heroes.Find(h => h.Name == heroName);          
+
+            if (hero.Mana >= spellMana)
+            {
+                hero.Mana -= spellMana;
+                Console.WriteLine($"{hero.Name} has successfully cast {spellName} and now has {hero.Mana} MP!");
+            }
+            else
+            {
+                Console.WriteLine($"{heroName} does not have enough MP to cast {spellName}!");
+            }
+
+        }
+
+        private static void TakeDamage(string heroName, int damage, string attacker, List<Hero> heroes)
+        {
+            Hero hero = heroes.Find(h => h.Name == heroName);
+
+            if (hero.Health > damage)
+            {
+                hero.Health -= damage;
+                Console.WriteLine($"{hero.Name} was hit for {damage} HP by {attacker} and now has {hero.Health} HP left!");
+            }
+            else
+            {              
+                Console.WriteLine($"{hero.Name} has been killed by {attacker}!");
+                heroes.Remove(hero);
+            }
+        }
+
+        private static void Recharge(string heroName, int manaPotion, List<Hero> heroes)
+        {
+            const int MAX_MANA = 200;
+
+            Hero hero = heroes.Find(h => h.Name == heroName);
+
+            int initialMana = hero.Mana;
+            hero.Mana += manaPotion;
+            if (hero.Mana >= MAX_MANA)
+            {
+                hero.Mana = MAX_MANA;
+            }
+            Console.WriteLine($"{hero.Name} recharged for {hero.Mana - initialMana} MP!");
+        }
+
+        private static void Heal(string heroName, int healthPotion, List<Hero> heroes)
+        {
+            const int MAX_HEALTH = 100;
+
+            Hero hero = heroes.Find(h => h.Name == heroName);
+
+            int initialHealth = hero.Health;
+            hero.Health += healthPotion;
+            if (hero.Health >= MAX_HEALTH)
+            {
+                hero.Health = MAX_HEALTH;
+            }
+            Console.WriteLine($"{hero.Name} healed for {hero.Health - initialHealth} HP!");
+        }
+    }
+
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+    }
+}
---
+++ Fundamentals/0_4_F_FinalExam Exercises/3. Plant Discovery Objects/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Plant_Discovery_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Plant> plants = new List<Plant>();
+
+            int inputLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] plantData = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+                string plantName = plantData[0];
+                int plantRarity = int.Parse(plantData[1]);
+
+                Plant plant = new Plant()
+                {
+                    Name = plantName,
+                    Rarity = plantRarity,
+                    TotalRating = 0
+                };
+
+                if (plant == plants.Find(p => p.Name == plantName))
+                {
+                    plant.Rarity = plantRarity;
+                }
+                else
+                {
+                    plants.Add(plant);
+                }
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Exhibition")
+                {
+                    break;
+                }
+                string[] inputData = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = inputData[0];
+                string plantName = inputData[1];
+
+                if (plants.Any(p => p.Name == plantName))
+                {
+                    switch (action)
+                    {
+                        case "Rate:":
+                            Rate(plants, inputData, plantName);
+                            break;
+                        case "Update:":
+                            Update(plants, inputData, plantName);
+                            break;
+                        case "Reset:":
+                            Reset(plants, plantName);
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("error");                    
+                }
+
+            }
+
+            Console.WriteLine("Plants for the exhibition:");
+            foreach (Plant item in plants)
+            {
+                Console.WriteLine($"- {item.Name}; Rarity: {item.Rarity}; Rating: {item.AverageRating():f2}");
+            }
+        }
+
+        private static void Rate(List<Plant> plants, string[] inputData, string plantName)
+        {
+            double rate = double.Parse(inputData[3]);
+            Plant plant = plants.Find(p => p.Name == plantName);
+
+            plant.RateCounter++;
+            plant.TotalRating += rate;
+        }
+
+        private static void Update(List<Plant> plants, string[] inputData, string plantName)
+        {
+            int newRarity = int.Parse(inputData[3]);
+            Plant plant = plants.Find(p => p.Name == plantName);
+
+            plant.Rarity = newRarity;
+        }
+
+        private static void Reset(List<Plant> plants, string plantName)
+        {
+            Plant plant = plants.Find(p => p.Name == plantName);
+
+            plant.TotalRating = 0.0;
+            plant.RateCounter = 0;
+        }
+
+    }
+    public class Plant
+    {
+        public string Name { get; set; }
+        public int Rarity { get; set; }
+        public double RateCounter { get; set; }
+        public double TotalRating { get; set; }
+        public double AverageRating()
+        {
+            if (RateCounter > 0 && TotalRating > 0)
+            {
+                return TotalRating / RateCounter;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
