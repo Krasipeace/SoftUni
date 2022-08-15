93ba377496a0d39f6f4b133c68f71ecb720bfea1
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-16 02:24:17 +0300

    old repo added

---
+++ Programming Basics/First Steps in Coding - Exercise/DepositCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //четем от конзолата 3 променливи
+            //1.Депозирана сума – реално число в интервала[100.00 … 10000.00]
+            //2.Срок на депозита(в месеци) – цяло число в интервала[1…12]
+            //3.Годишен лихвен процент (ГЛП) – реално число в интервала[0.00 …100.00]
+            double deposit = double.Parse(Console.ReadLine());
+            int monthDeposit = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+            //изчисление натрупаната лихва - депозирана сума * ГЛП  
+            double allInterest = deposit * interest;
+            //изчисляваме лихвата за 1 месец - НЛ / 12
+            double monthInterest = allInterest / 12 /100;
+            // изчисление на общата сума
+            double allSum = deposit + monthDeposit * monthInterest;
+            //отпечатваме общата сума - ДС + СнД * (лихва_за_месец)
+            Console.WriteLine(allSum);        
+        }
+    }
+}
---
+++ Programming Basics/First Steps in Coding - Exercise/VacationBooksList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //брой страници - цяло число
+            //прочетени страници за 1 час - цяло число
+            //броят дни за книга
+            int pages = int.Parse(Console.ReadLine());
+            int readpages = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            //изчисление общо време за четене на книга
+            int hoursPerBook = pages / readpages;
+            //изчисление на необходими часове на ден
+            int hoursPerDay = hoursPerBook / days;
+            //отпечатване на НЧнД
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
