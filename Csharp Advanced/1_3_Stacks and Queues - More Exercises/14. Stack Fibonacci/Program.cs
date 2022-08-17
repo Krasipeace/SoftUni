a4c09f48916d595389e1f9b4026710b0a0db1dc7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-17 19:54:41 +0300

    stack

---
+++ Csharp Advanced/1_3_Stacks and Queues - More Exercises/14. Stack Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14._Stack_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = int.Parse(Console.ReadLine());
+
+            Stack<ulong> fibonacci = new Stack<ulong>();
+
+            fibonacci.Push(1);
+            fibonacci.Push(1);
+
+            for (int i = 0; i < index - 1; i++)
+            {
+
+                if (fibonacci.Count == index)
+                {
+                    break;
+                }
+
+                ulong fibFirst = fibonacci.Pop();
+                ulong fibSecond = fibonacci.Peek();
+
+                fibonacci.Push(fibFirst);
+                fibonacci.Push(fibFirst + fibSecond);
+            }
+            Console.WriteLine(fibonacci.Pop());
+        }
+    }
+}
---
+++ Csharp Advanced/1_3_Stacks and Queues - More Exercises/16. Poisonous Plants/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16._Poisonous_Plants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputPlants = int.Parse(Console.ReadLine());
+
+            var plants = Console.ReadLine().Split(' ').Select(int.Parse).Take(inputPlants).ToList();
+
+            Stack<int> deadPositions = new Stack<int>();
+
+            int dayCounter = 0;
+
+            while (true)
+            {
+                dayCounter++;
+
+                int lastDayCounter = 0;
+
+                for (int i = 0; i < inputPlants - 1; i++)
+                {
+                    if (plants[i + 1] > plants[i])
+                    {
+                        deadPositions.Push(i + 1);
+
+                        lastDayCounter = 1;
+                    }
+                }
+
+                int deadCounter = deadPositions.Count;
+
+                for (int i = 0; i < deadCounter; i++)
+                {
+                    plants.RemoveAt(deadPositions.Pop());
+                }
+
+                inputPlants = plants.Count;
+
+                if (lastDayCounter == 0)
+                {
+                    Console.WriteLine(dayCounter - 1);
+
+                    return;
+                }
+            }
+        }
+    }
+}
