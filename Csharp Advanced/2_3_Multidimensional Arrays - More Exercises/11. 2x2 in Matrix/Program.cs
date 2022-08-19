84925b3a47e5498eb0eb36c3cb7de103bb5f5bc7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-20 00:50:47 +0300

    matrix

---
+++ Csharp Advanced/2_3_Multidimensional Arrays - More Exercises/11. 2x2 in Matrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._2x2_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_SQUARE_ROWS = 2;
+            const int MAX_SQUARE_COLUMNS = 2;
+
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            int counter = 0;
+            
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row + MAX_SQUARE_ROWS - 1 < rows && col + MAX_SQUARE_COLUMNS - 1 < cols)
+                    {
+                        char symbol = matrix[row, col];
+                        if (matrix[row, col] == symbol && matrix[row, col + 1] == symbol && matrix[row + 1, col] == symbol && matrix[row + 1,col + 1] == symbol)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ Csharp Advanced/2_3_Multidimensional Arrays - More Exercises/12. Max Sum/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._Max_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_SQUARE_ROWS = 3;
+            const int MAX_SQUARE_COLUMNS = 3;
+
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxRowIndex = 0;
+            int maxColIndex = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (row + MAX_SQUARE_ROWS - 1 < rows && col + MAX_SQUARE_COLUMNS - 1 < cols)
+                    {
+                        int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1] 
+                                  + matrix[row + 2, col] + matrix[row, col + 2] + matrix[row + 2, col + 2] + matrix[row + 1, col + 2] + matrix[row + 2, col + 1];
+
+                        if (sum > maxSum)
+                        {
+                            maxSum = sum;
+                            maxRowIndex = row;
+                            maxColIndex = col;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = maxRowIndex; row < maxRowIndex + MAX_SQUARE_ROWS; row++)
+            {
+                for (int cow = maxColIndex; cow < maxColIndex + MAX_SQUARE_COLUMNS; cow++)
+                {
+                    Console.Write($"{matrix[row, cow]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
