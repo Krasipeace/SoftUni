2d1040357541f91183d323e351518efb8a12eac5
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-24 12:38:16 +0300

    queue

--- Csharp Advanced/1_1_ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
+++ Csharp Advanced/1_1_ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _4._Matching_Brackets
 {
@@ -6,7 +7,22 @@ namespace _4._Matching_Brackets
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    brackets.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    int firstBracket = brackets.Pop();
+                    int lastBracket = i;
+                    Console.WriteLine(input.Substring(firstBracket, lastBracket - firstBracket + 1));
+                }
+            }
         }
     }
 }
---
+++ Csharp Advanced/1_1_ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    queue.Enqueue(input);
+                }
+                else if (input == "Paid")
+                {
+                    while (queue.Any())
+                    {
+                        Console.WriteLine($"{queue.Dequeue()}");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
