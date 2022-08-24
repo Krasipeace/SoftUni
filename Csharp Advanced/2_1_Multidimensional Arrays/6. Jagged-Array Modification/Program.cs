292125690cdcfc5649ad4cbe701881f970f17948
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-24 14:06:16 +0300

    jagged array

---
+++ Csharp Advanced/2_1_Multidimensional Arrays/6. Jagged-Array Modification/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] col = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                jaggedArray[row] = col;
+            }
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] input = command.Split(" ");
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+                int value = int.Parse(input[3]);
+
+                if (row < 0 || col < 0 || row >= jaggedArray.Length || col >= jaggedArray[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else if (input[0] == "Add")
+                {
+                    jaggedArray[row][col] += value;
+                }
+                else if (input[0] == "Subtract")
+                {
+                    jaggedArray[row][col] -= value;
+                }
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine($"{string.Join(" ", jaggedArray[i])}");
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/2_1_Multidimensional Arrays/7. Pascal Triangle/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputHeight = int.Parse(Console.ReadLine());
+
+            ulong[][] triangle = new ulong[inputHeight][];
+
+            for (int row = 0; row < inputHeight; row++)
+            {
+                triangle[row] = new ulong[row + 1];
+
+                for (int col = 0; col < row + 1; col++)
+                {
+                    ulong sum = 0;
+                    if (row - 1 >= 0 && col < triangle[row - 1].Length)
+                    {
+                        sum += triangle[row - 1][col];
+                    }
+
+                    if (row - 1 >= 0 && col - 1 >= 0)
+                    {
+                        sum += triangle[row - 1][col - 1];
+                    }
+
+                    if (sum == 0)
+                    {
+                        sum = 1;
+                    }
+
+                    triangle[row][col] = sum;
+                }
+            }
+
+            for (int row = 0; row < triangle.Length; row++)
+            {
+                for (int col = 0; col < triangle[row].Length; col++)
+                {
+                    Console.Write($"{triangle[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
