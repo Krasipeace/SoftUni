784a3026eafb01f83b344cc728bb9e4fee90319a
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-26 16:50:55 +0300

    files and directories

---
+++ Csharp Advanced/4_1_Streams Files and Directories/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,43 @@
+﻿namespace ExtractSpecialBytes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class ExtractSpecialBytes //unfinished
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            using StreamReader streamReader = new StreamReader(bytesFilePath);
+            byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+            var bytesList = new List<String>();
+            var stringBuilder = new StringBuilder();
+
+            while (!streamReader.EndOfStream)
+            {
+                bytesList.Add(streamReader.ReadLine());
+            }
+
+            foreach (var item in fileBytes)
+            {
+                if (bytesList.Contains(item.ToString()))
+                {
+                    stringBuilder.AppendLine(Convert.ToString(item, 16));
+                }
+            }
+
+            using StreamWriter output = new StreamWriter(outputPath);
+            output.WriteLine(stringBuilder.ToString().Trim().ToUpper());         
+        }
+    }
+}
---
+++ Csharp Advanced/4_1_Streams Files and Directories/FolderSize/FolderSize.cs
@@ -0,0 +1,29 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            double sum = 0;
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+            FileInfo[] files = directoryInfo.GetFiles("*", SearchOption.AllDirectories);
+
+            foreach (FileInfo fileInfo in files)
+            {
+                sum += fileInfo.Length;
+            }
+            sum /= 1024;
+            File.WriteAllText(outputFilePath, sum.ToString());           
+        }
+    }
+}
