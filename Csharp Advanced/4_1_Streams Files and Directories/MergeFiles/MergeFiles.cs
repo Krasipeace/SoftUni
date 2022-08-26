a1b55ea0aac1b25d6d3c70a6e1c5cbdc02e603b9
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-26 15:37:42 +0300

    read/write txt

---
+++ Csharp Advanced/4_1_Streams Files and Directories/MergeFiles/MergeFiles.cs
@@ -0,0 +1,46 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            var firstFileReader = new StreamReader(firstInputFilePath);
+            var secondFileReader = new StreamReader(secondInputFilePath);
+            var outputWriter = new StreamWriter(outputFilePath);
+
+            using (outputWriter)
+            {
+                using (firstFileReader)
+                {
+                    using (secondFileReader)
+                    {
+                        string line1 = null;
+                        string line2 = null;
+
+                        while ((line1 = firstFileReader.ReadLine()) != null)
+                        {
+                            outputWriter.WriteLine(line1);
+                            line2 = secondFileReader.ReadLine();
+
+                            if (line2 != null)
+                            {
+                                outputWriter.WriteLine(line2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/4_1_Streams Files and Directories/WordCount/WordCount.cs
@@ -0,0 +1,76 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    public class WordCount  //unfinished
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            var textReader = new StreamReader(textFilePath);
+            var wordReader = new StreamReader(wordsFilePath);
+
+            var wordsCounter = new Dictionary<string, int>();
+
+            using (wordReader)
+            {
+                string line = wordReader.ReadLine();
+
+                while (line != null)
+                {
+                    string[] lineWords = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < lineWords.Length; i++)
+                    {
+                        lineWords[i] = lineWords[i].ToLower();
+
+                        if (!(wordsCounter.ContainsKey(lineWords[i])))
+                        {
+                            wordsCounter.Add(lineWords[i], 0);
+                        }
+                    }
+                    line = wordReader.ReadLine();
+                }
+            }
+
+            var outputWriter = new StreamWriter(outputFilePath);
+
+            using (textReader)
+            {
+                string lineText = textReader.ReadLine().ToLower();
+
+                while (lineText != null)
+                {
+                    string[] lineTextSplited = lineText.Split(new char[] {' ', '\n', '.', '!', '?', '-', ','}, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int splittedWord = 0; splittedWord < lineTextSplited.Length; splittedWord++)
+                    {
+                        lineTextSplited[splittedWord] = lineTextSplited[splittedWord].ToLower();
+
+                        if (wordsCounter.ContainsKey(lineTextSplited[splittedWord]))
+                        {
+                            wordsCounter[lineTextSplited[splittedWord]] += 1;
+                        }
+                    }
+
+                    lineText = textReader.ReadLine();
+                }
+
+                foreach (var kvp in wordsCounter.OrderByDescending(k => k.Value))
+                {
+                    outputWriter.WriteLine($"{kvp.Key} - {kvp.Value}");  
+                }
+            }
+        }
+    }
+}
