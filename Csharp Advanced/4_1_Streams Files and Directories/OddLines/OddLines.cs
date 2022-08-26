924407d8cd5cb0a7a8966722774b488e9e859aae
Krasimir Dramaliev <krasipeace@abv.bg>
2022-08-26 13:06:19 +0300

    read write txt

---
+++ Csharp Advanced/4_1_Streams Files and Directories/OddLines/OddLines.cs
@@ -0,0 +1,38 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (var write = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            write.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
