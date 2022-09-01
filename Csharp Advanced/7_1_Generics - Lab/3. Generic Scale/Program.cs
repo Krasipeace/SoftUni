2acfc6779339e7f9dc2ce90f8babb610161b85f8
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-01 20:31:34 +0300

    Generics

---
+++ Csharp Advanced/7_1_Generics - Lab/3. Generic Scale/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+            int left = int.Parse(Console.ReadLine());
+            int right = int.Parse(Console.ReadLine());
+
+            EqualityScale<int> equality = new EqualityScale<int>(left,right);
+            
+            Console.WriteLine(equality.AreEqual());
+        }
+    }
+}
