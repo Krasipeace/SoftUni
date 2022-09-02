21d61af2545137ccfc377a3da9b75389285cc462
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-02 17:48:21 +0300

    basic algorithms

---
+++ Csharp Advanced/9_1_Basic Algorithms/1. Recursive Array Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int index = 0;
+           
+            Console.WriteLine(Sum(numbers,index));
+        }
+
+        private static int Sum(int[] numbers, int index)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);
+        }
+    }
+}
---
+++ Csharp Advanced/9_1_Basic Algorithms/2. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong number = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        private static ulong Factorial(ulong number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
