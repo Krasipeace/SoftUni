e72587b5dded8479b63d8b4ec516567424ba0287
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-05 16:18:56 +0300

    bin search v1.0

---
+++ Csharp Advanced/9_1_Basic Algorithms/7. Binary Search/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int key = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(array, key));
+        }
+
+        public static int BinarySearch(int[] array, int key)
+        {
+            int left = 0;
+            int right = array.Length;
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                if (array[mid] == key)
+                {
+                    return mid;
+                }
+                else if (array[mid] > key)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
