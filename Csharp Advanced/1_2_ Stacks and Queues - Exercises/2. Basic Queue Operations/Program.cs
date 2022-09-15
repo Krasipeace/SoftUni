afc56ffa3e1fffa50f578c3273cf8268413feafe
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-15 15:28:49 +0300

    queue

---
+++ Csharp Advanced/1_2_ Stacks and Queues - Exercises/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int n = input[0]; //enqueue n elements
+            int s = input[1]; //dequeue s elements
+            int x = input[2]; //check if element exist
+
+            Queue<int> queue = new Queue<int>();
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = numbers[i];
+                queue.Enqueue(currentNumber);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
