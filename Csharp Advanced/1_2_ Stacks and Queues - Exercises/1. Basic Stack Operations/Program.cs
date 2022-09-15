815561ceef0746a1722b38a3cb3802acc40d8a6a
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-15 15:18:51 +0300

    stack

---
+++ Csharp Advanced/1_2_ Stacks and Queues - Exercises/1. Basic Stack Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int n = input[0]; //push n elements
+            int s = input[1]; //pop s elements
+            int x = input[2]; //check if element exist 
+
+            Stack<int> stack = new Stack<int>();
+
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = numbers[i];
+                stack.Push(currentNumber);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
