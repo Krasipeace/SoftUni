d061207f5de628b46acd88cab6d7d9626068a18d
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-15 20:17:30 +0300

    queue

---
+++ Csharp Advanced/1_2_ Stacks and Queues - Exercises/4. Fast Food/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodAmount = int.Parse(Console.ReadLine());
+            int[] orders = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(orders);
+
+            Console.WriteLine(queue.Max());
+            while (queue.Count > 0)
+            {
+                if (queue.Peek() <= foodAmount)
+                {
+                    foodAmount -= queue.Peek();
+                    queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/1_2_ Stacks and Queues - Exercises/6. Songs Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> songList = new Queue<string>(songs);
+
+            string command = Console.ReadLine();
+            while (true)
+            {
+                string[] action = command.Split();
+
+                switch (action[0])
+                {
+                    case "Play":
+                        songList.Dequeue();
+                        break;
+                    case "Add":
+                        string songName = string.Join(" ", action.Skip(1));
+                        if (!songList.Contains(songName))
+                        {
+                            songList.Enqueue(songName);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{songName} is already contained!");
+                        }
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songList));
+                        break;
+                }
+
+                if (songList.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
