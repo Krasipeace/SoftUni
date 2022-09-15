c65b84ce5ebc12f0dca8d92ee8383d61307a12c7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-15 15:46:51 +0300

    stack

---
+++ Csharp Advanced/1_2_ Stacks and Queues - Exercises/3. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                int[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int action = command[0];
+
+                switch (action)
+                {
+                    case 1:
+                        stack.Push(command[1]);
+                        break;
+                    case 2:
+                        if (stack.Count > 0)
+                        { 
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        break;
+                    case 3:
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        break;
+                    case 4: 
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
