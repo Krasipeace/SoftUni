db8f0ccea8b322c8e838eac30760e5da226ce41a
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-20 11:44:25 +0300

    matrix

---
+++ Csharp Advanced/2_2_Multidimensional Arrays - Exercises/5. Snake Moves/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(m => int.Parse(m)).ToArray();
+            string snake = Console.ReadLine();
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+            char[,] matrix = new char[rows, cols];
+
+            int currentSnakeIndex = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (row % 2 != 0)
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        if (currentSnakeIndex == snake.Length)
+                        {
+                            currentSnakeIndex = 0;
+                        }
+
+                        matrix[row, col] = snake[currentSnakeIndex];
+
+                        currentSnakeIndex++;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        if (currentSnakeIndex == snake.Length)
+                        {
+                            currentSnakeIndex = 0;
+                        }
+
+                        matrix[row, col] = snake[currentSnakeIndex];
+
+                        currentSnakeIndex++;
+                    }
+                }
+            }
+            PrintResult(matrix);
+        }
+
+        static void PrintResult(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Csharp Advanced/2_2_Multidimensional Arrays - Exercises/7. Knight Game/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace _7._Knight_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            if (matrixSize < 3)
+            {
+                Console.WriteLine(0);
+
+                return;
+            }
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string chars = Console.ReadLine();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = chars[col];
+                }
+            }
+
+            int knightsRemoved = 0;
+
+            while (true)
+            {
+                int countMostAttacking = 0;
+                int rowMostAttacking = 0;
+                int colMostAttacking = 0;
+
+                for (int row = 0; row < matrixSize; row++)
+                {
+                    for (int col = 0; col < matrixSize; col++)
+                    {
+                        if (matrix[row, col] == 'K')
+                        {
+                            int attackedKnights = CountAttackedKnights(row, col, matrixSize, matrix);
+
+                            if (countMostAttacking < attackedKnights)
+                            {
+                                countMostAttacking = attackedKnights;
+                                rowMostAttacking = row;
+                                colMostAttacking = col;
+                            }
+                        }
+                    }
+                }
+
+                if (countMostAttacking == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    matrix[rowMostAttacking, colMostAttacking] = '0';
+                    knightsRemoved++;
+                }
+            }
+
+            Console.WriteLine(knightsRemoved);
+        }
+
+        static int CountAttackedKnights(int row, int col, int matrixSize, char[,] matrix)
+        {
+            int attackedKnights = 0;
+
+            if (isKnightMovingInTheMatrix(row - 1, col - 2, matrixSize))
+            {
+                if (matrix[row - 1, col - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row + 1, col - 2, matrixSize))
+            {
+                if (matrix[row + 1, col - 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row - 1, col + 2, matrixSize))
+            {
+                if (matrix[row - 1, col + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row + 1, col + 2, matrixSize))
+            {
+                if (matrix[row + 1, col + 2] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row - 2, col - 1, matrixSize))
+            {
+                if (matrix[row - 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row - 2, col + 1, matrixSize))
+            {
+                if (matrix[row - 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row + 2, col - 1, matrixSize))
+            {
+                if (matrix[row + 2, col - 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            if (isKnightMovingInTheMatrix(row + 2, col + 1, matrixSize))
+            {
+                if (matrix[row + 2, col + 1] == 'K')
+                {
+                    attackedKnights++;
+                }
+            }
+
+            return attackedKnights;
+        }
+
+        static bool isKnightMovingInTheMatrix(int row, int col, int matrixSize)
+        {
+            return row >= 0 && row < matrixSize && col >= 0 && col < matrixSize;
+        }
+    }
+}
