c3c601315b068ad0d2cdea316f6a5e95fc9d7621
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-21 14:43:24 +0300

    hashset

---
+++ Csharp Advanced/3_2_Sets And Dictionaries Adv - Exercises/1. Unique Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < inputs; i++)
+            {
+                string uniqueName = Console.ReadLine();
+                names.Add(uniqueName);
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
