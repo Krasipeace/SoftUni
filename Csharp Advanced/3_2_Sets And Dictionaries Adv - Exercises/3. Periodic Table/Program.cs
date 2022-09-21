a2359ef10e402d017e2ac65008cb305486e4e5b7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-21 15:50:02 +0300

    sorted set

---
+++ Csharp Advanced/3_2_Sets And Dictionaries Adv - Exercises/3. Periodic Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int linesOfInputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < linesOfInputs; i++)
+            {
+                string[] inputLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var unique in inputLine)
+                {
+                    elements.Add(unique);
+                }
+            }
+
+            foreach (var element in elements)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
