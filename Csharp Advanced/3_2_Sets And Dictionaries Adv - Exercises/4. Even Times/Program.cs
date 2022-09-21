20e59453a9e0ccf01a303ba7f1b65ad5d3bb92c7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-21 16:34:32 +0300

    dictionary

---
+++ Csharp Advanced/3_2_Sets And Dictionaries Adv - Exercises/4. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            int inputNumbers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputNumbers; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!dictionary.ContainsKey(number))
+                {
+                    dictionary.Add(number, 0);
+                }
+                dictionary[number]++;
+            }
+
+            foreach (var num in dictionary)
+            {
+                if (num.Value %  2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+
+                    return;
+                }
+            }
+
+        }
+    }
+}
