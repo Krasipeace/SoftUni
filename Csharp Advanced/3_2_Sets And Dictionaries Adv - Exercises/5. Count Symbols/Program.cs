0ae842e39f5a42ae04496c4c661c309e9846d57b
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-21 16:54:49 +0300

    sorted dictionary

---
+++ Csharp Advanced/3_2_Sets And Dictionaries Adv - Exercises/5. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> dictionary = new SortedDictionary<char, int>();
+
+            string input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!dictionary.ContainsKey(input[i]))
+                {
+                    dictionary.Add(input[i], 0);
+                }
+                dictionary[input[i]]++;
+
+            }
+
+            foreach (var symbol in dictionary)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
