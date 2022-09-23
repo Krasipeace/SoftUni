4de0661485b7bfee5d8c52f046b5c95268acbf58
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-23 22:07:14 +0300

    files

---
+++ Csharp Advanced/4_2_Streams Files and Directories - Exercises/LineNumbers/LineNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] allTextLines = File.ReadAllLines(inputFilePath);
+            int lineCounter = 1;
+            foreach (var (currentLine, letterCount, signsCount) in from currentLine in allTextLines
+                                                                       let letterCount = currentLine.Count(char.IsLetter)
+                                                                       let puncSignsCount = currentLine.Count(char.IsPunctuation)
+                                                                       select (currentLine, letterCount, puncSignsCount))
+            {
+                File.AppendAllText(outputFilePath, $"Line {lineCounter}: {currentLine} ({letterCount})({signsCount})\n");
+                lineCounter++;
+            }
+        }
+    }
+}
