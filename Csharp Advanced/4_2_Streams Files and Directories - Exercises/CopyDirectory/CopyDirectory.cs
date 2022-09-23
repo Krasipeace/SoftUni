72eb6b652db925299739e20ebd0365928b3270bf
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-23 20:11:34 +0300

    copy dir

---
+++ Csharp Advanced/4_2_Streams Files and Directories - Exercises/CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,29 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(inputPath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(inputPath, outputPath));
+            }
+
+            foreach (string newPath in Directory.GetFiles(inputPath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(inputPath, outputPath), true);
+            }
+        }
+    }
+}
