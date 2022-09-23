ab49b102288574f4ecab58741a7513ceaefbaa05
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-23 21:19:18 +0300

    zip and extract

---
+++ Csharp Advanced/4_2_Streams Files and Directories - Exercises/ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,35 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFile, string zipArchiveFile)
+        {
+            ZipFile.CreateFromDirectory(inputFile, zipArchiveFile);
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using (ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Update))
+            {
+                archive.CreateEntryFromFile(fileName, "extracted.png");
+                archive.ExtractToDirectory(outputFilePath);
+            }
+        }
+    }
+}
