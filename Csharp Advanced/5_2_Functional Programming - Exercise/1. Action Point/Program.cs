04f5716e13a899e9b5eb96a39f1bfa2d7e2c61ad
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-24 16:52:50 +0300

    action<T>

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/1. Action Point/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            input.ForEach(delegate (string name)
+            {
+                Console.WriteLine(name);
+            });
+        }
+    }
+}
