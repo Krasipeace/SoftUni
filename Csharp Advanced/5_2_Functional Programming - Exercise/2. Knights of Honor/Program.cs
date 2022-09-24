c8258c4d55aae4bcd1067176328d750d0bdeb7d2
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-24 16:55:46 +0300

    delegate

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/2. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            input.ForEach(delegate (string name)
+            {
+                Console.WriteLine($"Sir {name}");
+            });
+        }
+    }
+}
