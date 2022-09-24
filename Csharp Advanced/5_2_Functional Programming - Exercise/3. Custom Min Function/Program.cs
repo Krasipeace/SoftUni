7cc609bbe75c85393192e5b21c4338aaccc9825f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-24 17:18:14 +0300

    func<T,T>

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/3. Custom Min Function/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray();
+
+            if (numbers.Any())
+            {
+                Console.WriteLine(((Func<int[], int>)(array =>
+                {
+                    int number = array[0];
+                    for (int i = 1; i < array.Length; i++)
+                    {
+                        if (number > array[i])
+                        {
+                            number = array[i];
+                        }
+                    }
+
+                    return number;
+                }))(numbers));
+            }
+        }
+    }
+}
