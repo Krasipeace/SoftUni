a27ab2bacc6b37b5a7657bff7ad9ccfc1e929b84
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-24 18:48:43 +0300

    even or odd

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/4. Find Even or Odds/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Find_Even_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(r => int.Parse(r)).ToArray();
+            int start = range[0];
+            int end = range[1];
+            string command = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+            switch (command)
+            {
+                case "even":
+                    IEnumerable even = numbers.Where(n => n % 2 == 0);
+                    foreach (var item in even)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    break;
+                case "odd":
+                    IEnumerable odd = numbers.Where(n => n % 2 != 0);
+                    foreach (var item in odd)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    break;
+            }          
+        }
+    }
+}
