fc5821faedcffd730ad87ae83d913d1a5bcbb1af
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-26 13:06:15 +0300

    func<T,T>

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/5. Applied Arithmetics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToList();
+            string command = Console.ReadLine();
+            List<int> output = new List<int>();
+            
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        output += inputNumbers.Select(n => n + 1).ToList();
+                        break;
+                    case "multiply":
+                        output = inputNumbers.Select(n => n * 2).ToList();
+                        break;
+                    case "subtract":
+                        output = inputNumbers.Select(n => n - 1).ToList();
+                        break;
+                    case "print":
+                        foreach (int num in output)
+                        {
+                            Console.Write($"{num} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
