84e89b6ce674055a9e60a5a149aa2e8dd8f2b2d7
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-26 15:07:53 +0300

    functions

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/7. Predicate For Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthName = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            Func<List<string>, List<string>> getLengthNames = list => list.Where(n => n.Length <= lengthName).ToList();
+            Action<List<string>> print = list => Console.WriteLine(string.Join("\n", list));
+
+            names = getLengthNames(names);
+            print(names);
+        }
+    }
+}
