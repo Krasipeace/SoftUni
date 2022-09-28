774a5073c8d8f11a50839e325e51ef4527c7acee
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-28 15:36:54 +0300

    functions

---
+++ Csharp Advanced/5_2_Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            Func<string, int, bool> isWordMorePoints = (word, asciiPoints) => word.ToCharArray().Select(ch => (int)ch).Sum() >= asciiPoints;
+            Func<List<string>, int, Func<string, int, bool>, string> wordBestPoints = (arr, asciiPoints, func) => arr.FirstOrDefault(word => func(word, asciiPoints));
+
+            Action<string> print = str => Console.WriteLine(str);
+
+            string printName = wordBestPoints(names, points, isWordMorePoints);
+            print(printName);
+        }
+    }
+}
