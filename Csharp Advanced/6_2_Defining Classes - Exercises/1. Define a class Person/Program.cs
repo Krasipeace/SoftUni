219edb7a9be8a2d5f62acb3ff2fe24bb78261b34
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-28 17:38:44 +0300

    define class

---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/1. Define a class Person/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person();
+            peter.Name = "Peter";
+            peter.Age = 20;
+            Console.WriteLine(peter.Name);
+            Console.WriteLine(peter.Age);
+
+            Person george = new Person();
+            george.Name = "George";
+            george.Age = 18;
+            Console.WriteLine(george.Name);
+            Console.WriteLine(george.Age);
+
+            Person jose = new Person();
+            jose.Name = "Jose";
+            jose.Age = 43;
+            Console.WriteLine(jose.Name);
+            Console.WriteLine(jose.Age);
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+    }
+}
