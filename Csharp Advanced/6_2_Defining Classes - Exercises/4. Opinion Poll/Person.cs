7ca01325dfdc6429d6f5ae0167c13290efa853f5
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-30 17:28:55 +0300

    ver. 0.00001337

---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/4. Opinion Poll/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public Person()
+        {
+            name = "No name";
+            age = 1;
+        }
+        public Person(int age) : this()
+        {
+            this.age = age;
+        }
+        public Person(string name, int age): this(age)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/4. Opinion Poll/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(tokens[0], int.Parse(tokens[1]));
+
+                people.Add(person);               
+            }
+
+            IOrderedEnumerable<Person> sortedPeople = people.Where(p => p.Age > 30).OrderBy(p => p.Name);
+
+            foreach (Person person in sortedPeople)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
