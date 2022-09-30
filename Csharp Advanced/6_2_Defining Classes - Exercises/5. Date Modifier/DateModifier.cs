c7102244bf6d9a1eedbd6be1d95a51ff6a98681f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-09-30 18:14:45 +0300

    ver. 0.000001337

---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/5. Date Modifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _5._Date_Modifier
+{
+    public class DateModifier
+    {
+        public long GetDaysBetweenTwoDates(string firstDateAsString, string secondDateAsString)
+        {
+            DateTime firstDate = DateTime.ParseExact(firstDateAsString, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact(secondDateAsString, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            return Math.Abs((firstDate - secondDate).Days);
+        }
+    }
+}
---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/5. Date Modifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5._Date_Modifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDateAsString = Console.ReadLine();
+            string secondDateAsString = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.GetDaysBetweenTwoDates(firstDateAsString, secondDateAsString));
+        }
+    }
+}
