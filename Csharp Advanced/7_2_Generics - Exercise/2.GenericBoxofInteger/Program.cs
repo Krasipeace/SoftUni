d12408dc933f591515bb9e8ca0e588ddf9160527
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 19:22:16 +0300

    generics

---
+++ Csharp Advanced/7_2_Generics - Exercise/2.GenericBoxofInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericBoxofInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
