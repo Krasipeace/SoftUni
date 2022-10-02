d91760bb14d3f4a9c8b007ee54588dcbf2d365df
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 19:16:35 +0300

    generics

---
+++ Csharp Advanced/7_2_Generics - Exercise/1.GenericBoxOfString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box.ToString());
+            }       
+        }
+    }
+}
