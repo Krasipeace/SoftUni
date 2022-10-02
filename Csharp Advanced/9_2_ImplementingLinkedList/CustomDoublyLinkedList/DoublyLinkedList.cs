12c125b123794f8aec257b12ed1bbbf0e4b4e158
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 23:32:29 +0300

    Implementing Doubly linked list

---
+++ Csharp Advanced/9_2_ImplementingLinkedList/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get; private set; }
+        private class ListNode
+        {
+            public int Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+            public ListNode(int value)
+            {
+                this.Value = value;
+            }
+        }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(element);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+        public void AddLast(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+        public int RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+            this.Count--;
+            
+            return firstElement;
+        }
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            int lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+            this.Count--;
+
+            return lastElement;
+        }
+        public void ForEach(Action<int> action)
+        {
+            ListNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }         
+        }
+        public int[] ToArray()
+        {
+            int[] array = new int[this.Count];
+            int counter = 0;
+            ListNode currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+
+            return array;
+        }
+
+    }
+
+}
---
+++ Csharp Advanced/9_2_ImplementingLinkedList/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                doublyLinkedList.AddFirst(i);
+            }
+
+            int[] arr = doublyLinkedList.ToArray();
+
+            doublyLinkedList.ForEach(n => Console.Write($"{n} "));
+
+            Console.WriteLine("\nAdded 17 at first Index: ");
+            doublyLinkedList.AddFirst(17);
+            doublyLinkedList.ForEach(n => Console.Write($"{n} "));
+
+            Console.WriteLine("\nAdded 69 at last Index: ");
+            doublyLinkedList.AddLast(69);
+            doublyLinkedList.ForEach(n => Console.Write($"{n} "));
+
+            Console.Write("\nFirst Removed Element is :");
+            Console.WriteLine(doublyLinkedList.RemoveFirst());
+            doublyLinkedList.ForEach(n => Console.Write($"{n} "));
+
+            Console.Write("\nLast Removed Element is :");
+            Console.WriteLine(doublyLinkedList.RemoveLast());
+
+            Console.WriteLine("----******************-----");
+            doublyLinkedList.ForEach(n => Console.Write($"{n} "));
+        }
+    }
+}
+
