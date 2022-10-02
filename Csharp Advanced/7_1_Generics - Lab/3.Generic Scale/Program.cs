9675447a6f800e0f0f82b94ca64035532b6fc361
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 15:05:29 +0300

    generics

--- Csharp Advanced/7_1_Generics - Lab/3.Generic Scale/Program.cs
+++ Csharp Advanced/7_1_Generics - Lab/3.Generic Scale/Program.cs
@@ -5,12 +5,12 @@ namespace GenericScale
     public class StartUp
     {
         static void Main(string[] args)
-        {            
+        {
             int left = int.Parse(Console.ReadLine());
             int right = int.Parse(Console.ReadLine());
 
-            EqualityScale<int> equality = new EqualityScale<int>(left,right);
-            
+            EqualityScale<int> equality = new EqualityScale<int>(left, right);
+
             Console.WriteLine(equality.AreEqual());
         }
     }
