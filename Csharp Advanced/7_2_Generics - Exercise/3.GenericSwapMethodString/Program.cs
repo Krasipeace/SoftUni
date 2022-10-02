74a670acc25364f46aad9b68c029aba97b38b2e1
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 20:00:19 +0300

    generic method Swap

---
+++ Csharp Advanced/7_2_Generics - Exercise/3.GenericSwapMethodString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                boxes.Add(box);
+            }
+
+            int[] swapIndex = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(s => int.Parse(s)).ToArray();
+            int firstBox = swapIndex[0];
+            int secondBox = swapIndex[1];
+
+            Swap(boxes, firstBox, secondBox);
+
+            foreach (var item in boxes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Swap<T>(List<Box<T>> boxes, int firstBox, int secondBox)
+        {
+            Box<T> swapTemp = boxes[firstBox];
+            boxes[firstBox] = boxes[secondBox];
+            boxes[secondBox] = swapTemp;
+        }
+    }
+}
---
+++ Csharp Advanced/7_2_Generics - Exercise/4.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        public T Type { get; }
+
+        public Box(T type)
+        {
+            this.Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type.GetType().FullName}: {Type}";
+        }
+    }
+}
