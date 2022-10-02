a7796381b52b57f41418e6a74aaf1417972387fc
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 23:33:50 +0300

    Update DoublyLinkedList.cs

--- Csharp Advanced/9_2_ImplementingLinkedList/CustomDoublyLinkedList/DoublyLinkedList.cs
+++ Csharp Advanced/9_2_ImplementingLinkedList/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CustomDoublyLinkedList
+namespace CustomDoublyLinkedList  // https://en.wikipedia.org/wiki/Doubly_linked_list
 {
     public class DoublyLinkedList
     {
