8fa9aa450be0f7b8910bc12423c08863f81c7ec9
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-02 14:50:10 +0300

    generics

---
+++ Csharp Advanced/7_1_Generics - Lab/1.Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data = new List<T>();
+        public int Count => this.data.Count;
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            var removedBox = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+
+            return removedBox;
+        }
+    }
+}
