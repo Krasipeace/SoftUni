10f294305bf33466dc3214389685883a69c0393c
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-03 16:09:49 +0300

    custom List implementations v.0.1

---
+++ Csharp Advanced/9_3_ImplementingStackAndQueue/1.ImplementingCustomListClass/CustomList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomListClass
+{
+    public class CustomList
+    {
+        private const int INITIAL_CAPACITY = 2;
+        private int[] items;
+        public CustomList()
+        {
+            this.items = new int[INITIAL_CAPACITY];
+        }
+        public int Count { get; private set; }
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return items[index];
+            }
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                items[index] = value;
+            }
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[this.items.Length * 2];
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copy[i] = this.items[i];
+            }
+            this.items = copy;
+        }
+        public void Add(int item)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+        }
+        private void Shrink()
+        {
+            int[] copy = new int[this.items.Length / 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+            this.items = copy;
+        }
+        public int RemoveAt(int index)
+        {
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int item = this.items[index];
+            this.items[index] = default(int);
+            this.Shift(index);
+            this.Count--;
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+
+            return item;
+        }
+
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+        }
+        public void Insert(int index, int element)
+        {
+            if (index > this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            ShiftToRight(index);
+            this.items[index] = element;
+            this.Count++;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                int item = this.items[i];
+                if (item.Equals(element))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            int swappedIndex = this.items[firstIndex];
+            if (firstIndex >= 0 && firstIndex < this.Count && secondIndex >= 0 && secondIndex < this.Count)
+            {
+                this.items[firstIndex] = this.items[secondIndex];
+                this.items[secondIndex] = swappedIndex;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{string.Join(" ", items)}";
+        }
+    }
+}
---
+++ Csharp Advanced/9_3_ImplementingStackAndQueue/1.ImplementingCustomListClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomListClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+            list.Add(1);
+        }
+    }
+}
