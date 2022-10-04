210a0fb1063ae28117fa52216eddfb0c82d784ba
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-04 13:19:29 +0300

    classes

---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/7.RawData/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    public class Car
+    {
+
+        public Car(string model, int speed, int power,
+                   int cargoWeight, string cargoType,
+                   double tire1pressure, int tire1age,
+                   double tire2pressure, int tire2age,
+                   double tire3pressure, int tire3age,
+                   double tire4pressure, int tire4age)
+        {
+            Model = model;
+            Engine = new Engine { Speed = speed, Power = power };
+            Cargo = new Cargo { CargoWeight = cargoWeight, CargoType = cargoType };
+            Tires = new Tire[4];
+            Tires[0] = new Tire { Pressure = tire1pressure, Age = tire1age };
+            Tires[1] = new Tire { Pressure = tire2pressure, Age = tire2age };
+            Tires[2] = new Tire { Pressure = tire3pressure, Age = tire3age };
+            Tires[3] = new Tire { Pressure = tire4pressure, Age = tire4age };
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+
+    public class Engine
+    {
+        public int Speed { get; set; }
+        public int Power { get; set; }
+    }
+
+    public class Cargo
+    {
+        public string CargoType { get; set; }
+        public int CargoWeight { get; set; }
+    }
+
+    public class Tire
+    {
+        public double Pressure { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Csharp Advanced/6_2_Defining Classes - Exercises/7.RawData/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RawData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int inputs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputs; i++)
+            {
+                string[] carData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Car car = new Car(
+                    carData[0],                                                 //Model
+                    int.Parse(carData[1]), int.Parse(carData[2]),               //Engine
+                    int.Parse(carData[3]), carData[4],                          //Cargo
+                    double.Parse(carData[5]), int.Parse(carData[6]),            //Tires
+                    double.Parse(carData[7]), int.Parse(carData[8]),
+                    double.Parse(carData[9]), int.Parse(carData[10]),
+                    double.Parse(carData[11]), int.Parse(carData[12])
+                    );
+
+                cars.Add(car);
+            }
+
+            List<string> filter = GetStatusOfCar(cars);
+
+            PrintFilteredCar(filter);
+        }       
+
+        static List<string> GetStatusOfCar(List<Car> cars)
+        {
+            List<string> filter = new List<string>();
+            string status = Console.ReadLine();
+            switch (status)
+            {
+                case "fragile":
+                    {
+                        filter = cars
+                            .Where(c => c.Cargo.CargoType == "fragile" && c.Tires.Any(t => t.Pressure < 1))
+                            .Select(c => c.Model)
+                            .ToList();
+                        break;
+                    }
+
+                case "flammable":
+                    {
+                        filter = cars
+                            .Where(c => c.Cargo.CargoType == "flammable" && c.Engine.Power > 250)
+                            .Select(c => c.Model)
+                            .ToList();
+                        break;
+                    }
+            }
+
+            return filter;
+        }
+        static void PrintFilteredCar(List<string> filter)
+        {
+            foreach (var item in filter)
+            {
+                Console.WriteLine($"{string.Join(" ", item)}");
+            }
+        }
+    }
+}
