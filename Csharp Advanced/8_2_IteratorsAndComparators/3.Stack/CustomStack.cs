ef531e59517aa9a63b4433e448394b1f372744c6
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-12 15:51:59 +0300

    yield return

---
+++ Csharp Advanced/8_2_IteratorsAndComparators/3.Stack/CustomStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> data;
+        public int Count => data.Count;
+        public CustomStack()
+        {
+            this.data = new List<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = data.Count - 1; i >= 0; i--)
+            {
+                yield return this.data[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        public void Push(params T[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.data.Add(elements[i]);
+            }
+        }
+        public void Pop()
+        {
+            this.data.RemoveAt(data.Count - 1);
+        }
+    }
+}
---
+++ Csharp Advanced/8_2_IteratorsAndComparators/3.Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> customStack = new CustomStack<string>();
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "Push":
+                        string[] addElement = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+                        customStack.Push(addElement);
+                        break;
+                    case "Pop":
+                        if (customStack.Count != 0)
+                        {
+                            customStack.Pop();
+                        }
+                        else
+                        {
+                            Console.WriteLine("No elements");
+                        }
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            PrintStack(customStack);
+        }
+
+        static void PrintStack(CustomStack<string> customStack)
+        {
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item.Replace(",", " "));
+            }
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item.Replace(",", " "));
+            }
+        }
+    }
+}
