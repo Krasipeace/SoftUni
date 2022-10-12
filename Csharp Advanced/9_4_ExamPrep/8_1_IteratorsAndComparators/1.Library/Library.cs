fdc732c0bee57cb35b42ad36f8eaf48b076625ac
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-12 13:24:54 +0300

    yield return

---
+++ Csharp Advanced/9_4_ExamPrep/8_1_IteratorsAndComparators/1.Library/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+         private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }       
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.books)
+            {
+                yield return book;
+            }
+        }       
+    }
+}
