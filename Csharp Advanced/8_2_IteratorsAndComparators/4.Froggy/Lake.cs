2d0d53d6081cbe210b05e545d0de471f58248313
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-12 16:06:58 +0300

    yield return

---
+++ Csharp Advanced/8_2_IteratorsAndComparators/4.Froggy/Lake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4.Froggy
+{
+    public class Lake<T> : IEnumerable<int>
+    {
+        private List<int> stones;
+        public Lake(params int[] stones)
+        {
+            this.stones = stones.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            int fromEndtoBegining = this.stones.Count % 2 == 0 ? this.stones.Count - 1 : this.stones.Count - 2;
+            for (int i = fromEndtoBegining; i >= 0; i -= 2)
+            {
+                yield return stones[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ Csharp Advanced/8_2_IteratorsAndComparators/4.Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray();
+            Lake<int> lake = new Lake<int>(input);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
