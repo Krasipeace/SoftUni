33aa74ce1237760df5a6d3ce3c075929d33fa83e
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-12 17:34:18 +0300

    func custom comparator

---
+++ Csharp Advanced/8_2_IteratorsAndComparators/7.CustomComparator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class CustomComparator
+    {
+        static void Main(string[] args)
+        {
+            int[] list = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Func<int, int, int> customComparator = (numberOne, numberTwo) =>
+            {
+                if (numberOne % 2 == 0 && numberTwo % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (numberOne % 2 != 0 && numberTwo % 2 == 0)
+                {
+                    return 1;
+                }
+
+                return numberOne.CompareTo(numberTwo);
+            };
+
+            Array.Sort(list, (numOne, numTwo) => customComparator(numOne, numTwo));
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
