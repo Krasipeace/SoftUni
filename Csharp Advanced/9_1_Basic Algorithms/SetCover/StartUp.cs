bcd9a012222ca4c38e6913aee56bbce41f66e215
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-13 15:36:25 +0300

    greedy

---
+++ Csharp Advanced/9_1_Basic Algorithms/SetCover/StartUp.cs
@@ -0,0 +1,58 @@
+﻿namespace SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp          //https://en.wikipedia.org/wiki/Set_cover_problem
+    {
+        static void Main(string[] args)
+        {
+            int[] universe = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            int numberOfSets = int.Parse(Console.ReadLine());
+            int[][] sets = new int[numberOfSets][];
+
+            for (int row = 0; row < sets.Length; row++)
+            {
+                int[] rowsValue = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+                sets[row] = new int[rowsValue.Length];
+
+                for (int col = 0; col < sets[row].Length; col++)
+                {
+                    sets[row][col] = rowsValue[col];
+                }
+            }
+
+            List<int[]> selectedSets = ChooseSets(sets.ToList(), universe.ToList());
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+
+            foreach (var item in selectedSets)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", item)} }}");
+            }
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+            List<int[]> resultSet = new List<int[]>();
+
+            HashSet<int> universeSet = new HashSet<int>(universe);
+            HashSet<int[]> selectedSet = new HashSet<int[]>(sets);
+
+            while (universeSet.Count > 0)
+            {
+                int[] currentSet = selectedSet.OrderByDescending(s => s.Count(e => universeSet.Contains(e))).First();
+
+                resultSet.Add(currentSet);
+                selectedSet.Remove(currentSet);
+
+                foreach (int number in currentSet)
+                {
+                    universeSet.Remove(number);
+                }
+            }
+
+            return resultSet;           
+        }
+    }
+}
