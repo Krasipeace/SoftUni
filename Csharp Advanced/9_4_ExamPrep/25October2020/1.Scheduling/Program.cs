db9039261bf16f3886131a7bcd4400c192759edd
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-14 13:57:18 +0300

    stack queue

---
+++ Csharp Advanced/9_4_ExamPrep/25October2020/1.Scheduling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.Scheduling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while (tasks.Peek() != taskToKill)
+            {
+                if (tasks.Peek() <= threads.Peek())
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else
+                {
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", threads));
+        }
+    }
+}
