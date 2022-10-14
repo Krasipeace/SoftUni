e72478a3fe474688cb25ff9b838349d57770144a
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-14 22:43:51 +0300

    classes

---
+++ Csharp Advanced/9_4_ExamPrep/16December2020/3.Openning/Bakery.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> employees;
+        private string name;
+        private int capacity;
+        public Bakery(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.employees = new List<Employee>(Capacity);
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return employees.Count; } }
+
+        public void Add(Employee employee)
+        {
+            if (employees.Count < Capacity)
+            {
+                employees.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Employee employee = employees.Find(e => e.Name == name);
+
+            if (employee != null)
+            {
+                employees.Remove(employee);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            Employee employee = employees.OrderByDescending(e => e.Age).First();
+
+            return employee;
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            Employee employee = employees.Find(e => e.Name == name);
+
+            return employee;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {this.Name}:");
+            foreach (var item in employees)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Csharp Advanced/9_4_ExamPrep/16December2020/3.Openning/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        private string name;
+        private int age;
+        private string country;       
+        public Employee(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;           
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string Country { get { return country; } set { country = value; } }        
+
+        public override string ToString()
+        {
+            return $"Employee: {this.Name}, {this.Age} ({this.Country})";
+        }
+    }
+}
