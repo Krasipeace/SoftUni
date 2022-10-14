bd490fc6e8513ffb9f593e683eec99f800d9763f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-14 16:54:38 +0300

    classes

---
+++ Csharp Advanced/9_4_ExamPrep/25October2020/3.ClassroomProject/Classroom.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+        private int capacity;
+        public Classroom(int capacity)
+        {
+            this.Students = new List<Student>();
+            this.Capacity = capacity;
+        }
+        public List<Student> Students { get { return students; } set { students = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return students.Count; } }
+
+        public string RegisterStudent(Student regStudent)
+        {
+            if (this.Count != this.Capacity)
+            {
+                this.Students.Add(regStudent);
+
+                return $"Added student {regStudent.FirstName} {regStudent.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            if (this.Students.Any(s => s.FirstName == firstName && s.LastName == lastName))
+            {
+                this.Students.Remove(this.Students.Find(s => s.FirstName == firstName && s.LastName == lastName));
+
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            if (!this.Students.Any(s => s.Subject == subject))
+            {
+                return "No students enrolled for the subject";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Subject: {subject}");
+            sb.AppendLine("Students:");
+
+            foreach (Student student in Students.Where(s => s.Subject == subject))
+            {
+                sb.AppendLine($"{student.FirstName} {student.LastName}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public int GetStudentsCount() => this.Count;
+
+        public Student GetStudent(string firstName, string lastName) => this.Students.Find(s => s.FirstName == firstName && s.LastName == lastName);
+    }
+}
---
+++ Csharp Advanced/9_4_ExamPrep/25October2020/3.ClassroomProject/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string subject;
+
+        public Student(string firstName, string lastName, string subject)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Subject = subject;
+        }
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public string Subject { get { return subject; } set { subject = value; } }
+
+        public override string ToString()
+            => $"Student: First Name = {this.FirstName}, Last Name = {this.LastName}, Subject = {this.Subject}";
+    }
+}
