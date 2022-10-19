262d546889bf13cdc84e1f2a0796d901c5460542
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-19 15:36:40 +0300

    stacks

---
+++ Csharp Advanced/9_4_ExamPrep/18August2021/1.BirthdayCelebration/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> guestsCapacity = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(g => int.Parse(g)).ToArray().Reverse());
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(p => int.Parse(p)).ToArray());
+
+            int wastedFood = 0;
+
+            while (guestsCapacity.Count > 0 && plates.Count > 0)
+            {
+                int currentGuests = guestsCapacity.Peek();
+                int currentPlates = plates.Peek();
+
+                currentGuests -= currentPlates;
+                guestsCapacity.Pop();
+                plates.Pop();
+
+                if (currentGuests > 0)
+                {
+                    guestsCapacity.Push(currentGuests);
+                }
+                else
+                {
+                    wastedFood += Math.Abs(currentGuests);
+                }
+            }
+            Console.WriteLine(plates.Count > 0 ? $"Plates: {string.Join(" ", plates)}" : $"Guests: {string.Join(" ", guestsCapacity)}");
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
