bf229ca3b1d93672ab951b6ab11f55fdd847a09c
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-24 00:45:02 +0300

    customQ

---
+++ Csharp Advanced/9_3_ImplementingStackAndQueue/3.ImplementingCustomQueueClass/CustomQueue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.ImplementingCustomQueueClass
+{
+    public class CustomQueue
+    {
+        private const int INITIAL_CAPACITY = 4;
+        private const int FIRST_ELEMENT_INDEX = 0;
+        private int[] elements;
+        private int count;
+
+        public CustomQueue()
+        {
+            count = 0;
+            elements = new int[INITIAL_CAPACITY];
+        }
+        public int Count => count;
+
+        public void Enqueue(int element)
+        {
+            if (elements.Length == count)
+            {
+                IncreaseLength();
+            }
+
+            elements[count] = element;
+            count++;
+        }
+        private void IncreaseLength()
+        {
+            int[] tempArray = new int[elements.Length * 2];
+            elements.CopyTo(tempArray, 0);
+            elements = tempArray;
+        }
+
+        public int Dequeue()
+        {
+            isValid();
+                       
+            int temp = elements[FIRST_ELEMENT_INDEX];
+            elements[FIRST_ELEMENT_INDEX] = elements[FIRST_ELEMENT_INDEX + 1];
+            count--;
+
+            if (count == 0)
+            {
+                elements[elements.Length - 1] = 0;
+            }
+
+            return temp;
+        }
+        private void isValid()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException($"No elements in the Queue!");
+            }
+        }
+
+        public int Peek()
+        {
+            return elements[FIRST_ELEMENT_INDEX];
+        }
+
+        public void Clear()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{string.Join(" ", elements)}";
+        }
+
+        
+    }
+}
---
+++ Csharp Advanced/9_3_ImplementingStackAndQueue/3.ImplementingCustomQueueClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3.ImplementingCustomQueueClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue customQueue = new CustomQueue();
+            customQueue.Enqueue(1);
+            customQueue.Enqueue(2);
+            customQueue.Enqueue(3);
+            Console.WriteLine(customQueue.ToString()); 
+
+            customQueue.Dequeue();
+            Console.WriteLine(customQueue.ToString());
+            customQueue.Enqueue(4);
+            Console.WriteLine(customQueue.ToString());
+            customQueue.Enqueue(5);
+            Console.WriteLine(customQueue.ToString());
+
+        }
+    }
+}
