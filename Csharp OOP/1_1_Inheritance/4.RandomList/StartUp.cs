66b2be8e86c6a4d09792eeedb568291b0127e58d
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-26 13:30:19 +0300

    inheritance collections

---
+++ Csharp OOP/1_1_Inheritance/4.RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings = new RandomList();
+
+            strings.Add("1");
+            strings.Add("2");
+            strings.Add("3");
+
+            Console.WriteLine(strings.RandomString());
+        }
+    }
+}
