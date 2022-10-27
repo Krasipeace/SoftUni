8c508e296c4401880b9b909cee566a0c4305408d
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-27 17:36:52 +0300

    homework: inheritance

---
+++ Csharp OOP/1_2_Inheritance/Zoo/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Lizard lizard = new Lizard("Goshko");
+            Snake snake = new Snake("Mariika");
+            Gorilla gorilla = new Gorilla("Kongi");
+            Bear bear = new Bear("Masha");
+        }
+    }
+}
