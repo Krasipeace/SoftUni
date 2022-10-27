165a92602fc69da74637df513f1b6293fb56a445
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-27 18:06:14 +0300

    homework: inheritance

---
+++ Csharp OOP/1_2_Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //sample code
+
+            MuseElf museElf = new MuseElf("Legolas", 69);
+            SoulMaster soulMaster = new SoulMaster("KelThuzad", 100);
+            BladeKnight bladeKnight = new BladeKnight("Illidan", 10000);
+
+            System.Console.WriteLine(bladeKnight.ToString());
+        }
+    }
+}
