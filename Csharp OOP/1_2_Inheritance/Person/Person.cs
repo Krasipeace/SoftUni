e3526dcc4d4dbb692cf57ab13f0b9be3e9b5e578
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-27 17:24:51 +0300

    homework: inheritance

---
+++ Csharp OOP/1_2_Inheritance/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format($"Name: {Name}, Age: {Age}"));
+
+            return sb.ToString();
+        }
+    }
+}
