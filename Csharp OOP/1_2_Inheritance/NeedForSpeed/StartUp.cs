25c6533ffea05b637667c5fa194582223a1fec31
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-28 15:33:35 +0300

    homework: inheritance

---
+++ Csharp OOP/1_2_Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car (70, 50);
+            car.Drive(10);
+
+            RaceMotorcycle raceMotorcycle = new RaceMotorcycle(500, 60);
+            raceMotorcycle.Drive(10);
+
+            SportCar sportCar = new SportCar(300, 70);
+            sportCar.Drive(10);
+           
+        }
+    }
+}
