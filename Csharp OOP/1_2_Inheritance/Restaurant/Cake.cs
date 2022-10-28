8786cc36063153d798edf713e2c0d0f26ec2ce7f
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-28 17:37:53 +0300

    homework: inheritance

---
+++ Csharp OOP/1_2_Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal CAKE_PRICE = 5m;
+        private const double CAKE_GRAMS = 250;
+        private const double CAKE_CALORIES = 1000;
+        public Cake(string name) : base(name, CAKE_PRICE, CAKE_GRAMS, CAKE_CALORIES)
+        {
+        }
+    }
+}
---
+++ Csharp OOP/1_2_Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double COFFEE_MILILLITERS = 50;
+        private const decimal COFFEE_PRICE = 3.5m;
+        public Coffee(string name, double caffeine) : base(name, COFFEE_PRICE, COFFEE_MILILLITERS)
+        {
+            Caffeine = caffeine;
+        }
+        public double Caffeine { get; set; }
+    }
+}
