61c53790dc848365bfb1de90de2cd7bc5dc3dbd5
Krasimir Dramaliev <krasipeace@abv.bg>
2022-10-31 16:51:45 +0200

    homework: Encapsulation

---
+++ Csharp OOP/2_2_Encapsulation/1.ClassBoxData/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                box.SurfaceArea();
+                box.LateralSurfaceArea();
+                box.Volume();
+
+                Console.WriteLine(box.ToString()); 
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }           
+        }
+    }
+}
