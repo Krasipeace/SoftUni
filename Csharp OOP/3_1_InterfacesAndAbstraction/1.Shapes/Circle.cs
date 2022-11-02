2eb2896bd20f22f37bf892b0c995a10171f9de5d
Krasimir Dramaliev <krasipeace@abv.bg>
2022-11-02 17:53:00 +0200

    interfaces and abstraction - lab

---
+++ Csharp OOP/3_1_InterfacesAndAbstraction/1.Shapes/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            private set
+            {
+                radius = value;
+            }
+        }
+
+    public void Draw()
+    {
+        double rIn = Radius - 0.4;
+        double rOut = Radius + 0.4;
+        for (double y = Radius; y >= -Radius; --y)
+        {
+
+            for (double x = -Radius; x < rOut; x += 0.5)
+            {
+                double value = x * x + y * y;
+
+                if (value >= rIn * rIn && value <= rOut * rOut)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+}
---
+++ Csharp OOP/3_1_InterfacesAndAbstraction/1.Shapes/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            DrawLine(Width, '*', '*');
+
+            for (int i = 1; i < Height; ++i)
+            {
+                DrawLine(Width, '*', ' ');
+            }
+
+            DrawLine(Width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+
+            Console.WriteLine(end);
+        }
+    }
+}
