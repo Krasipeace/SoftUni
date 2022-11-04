919772172b34e9f5ebb0781db8185a77a8b9dc1c
Krasimir Dramaliev <krasipeace@abv.bg>
2022-11-04 17:11:00 +0200

    Update StartUp.cs

--- Csharp OOP/3_2_InterfacesAndAbstraction/5.BirthdayCelebrations/StartUp.cs
+++ Csharp OOP/3_2_InterfacesAndAbstraction/5.BirthdayCelebrations/StartUp.cs
@@ -1,13 +1,7 @@
 ﻿namespace BorderControl
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
     using BorderControl.Core;
     using BorderControl.Core.Interfaces;
-    using Models;
-    using Models.Interfaces;
     
     public class StartUp
     {       
