78aee437f922abeff3d21ca562d18fec886b4dc4
Krasimir Dramaliev <krasipeace@abv.bg>
2022-11-10 17:12:14 +0200

    try-catch-finally

---
+++ Csharp OOP/7_1_ExceptionsAndErrorHandling/1.SquareRoot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                int number = int.Parse(Console.ReadLine());
+                int x = (int)Math.Sqrt(number);
+
+                if (x >= 0)
+                {
+                    Console.WriteLine(x);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number."); 
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
