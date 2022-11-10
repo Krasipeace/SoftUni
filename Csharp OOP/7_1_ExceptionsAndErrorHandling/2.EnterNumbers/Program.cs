da0be6ce2d50689f2327a33d9ea156ca3a7bdd30
Krasimir Dramaliev <krasipeace@abv.bg>
2022-11-10 18:03:03 +0200

    try-catch

---
+++ Csharp OOP/7_1_ExceptionsAndErrorHandling/2.EnterNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        int currentStart = 1;
+        int end = 100;
+        int[] output = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                output[i] = ReadNumber(currentStart, end);
+                currentStart = output[i];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine($"Your number is not in range {currentStart} - {end}!");
+                i--;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number!");
+                i--;
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", output));
+    }
+
+    static int ReadNumber(int start, int end)
+    {
+        int currentInput = int.Parse(Console.ReadLine());
+
+        if (currentInput <= start || currentInput >= end)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return currentInput;
+    }
+}
+
