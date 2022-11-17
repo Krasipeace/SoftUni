1a698511920314ec502b054126fa6d88c89089f1
Krasimir Dramaliev <krasipeace@abv.bg>
2022-11-17 15:43:11 +0200

    command pattern
    https://en.wikipedia.org/wiki/Command_pattern

---
+++ Csharp OOP/5_2_ReflectionAndAttributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,37 @@
+﻿namespace CommandPattern.Core
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Contracts;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string commandPostFix = "Command";
+
+        public string Read(string args)
+        {
+            string[] commandParts = args.Split(" ");
+            string commandName = commandParts[0];
+            string commandTypeName = commandName + commandPostFix;
+
+            Type commandType = Assembly.GetCallingAssembly().GetTypes()
+                .Where(t => t.GetInterfaces().Any(i => i.Name == nameof(ICommand)))
+                .FirstOrDefault(t => t.Name == commandTypeName);
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Command type is invalid!");
+            }
+
+            ICommand command = (ICommand)Activator.CreateInstance(commandType);
+
+            string[] commandData = commandParts.Skip(1).ToArray();
+            string result = command.Execute(commandData);
+
+            return result;
+        }
+
+    }
+}
---
+++ Csharp OOP/5_2_ReflectionAndAttributes/CommandPattern/StartUp.cs
@@ -0,0 +1,16 @@
+﻿// https://en.wikipedia.org/wiki/Command_pattern
+namespace CommandPattern
+{
+    using Core;
+    using Core.Contracts;  
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
+        }
+    }
+}
