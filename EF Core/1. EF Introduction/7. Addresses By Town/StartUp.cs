98331fa01e051812f2f09014450542aadfd5ef40
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 17:31:57 +0200

    task 7 completed

---
+++ EF Core/1. EF Introduction/7. Addresses By Town/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string result = GetAddressesByTown(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addressesByTown = context.Addresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town.Name)
+                .ThenBy(a => a.AddressText)
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town!.Name,
+                    EmployeesCount = a.Employees.Count
+                })
+                .Take(10)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var a in addressesByTown)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
