9e2b55e2b0a8639f6a63df883484266d449f658f
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 15:10:39 +0200

    problem 2 solved

---
+++ EF Core/1. EF Introduction/2. Employees Full Information/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })                   //Query optimization
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
