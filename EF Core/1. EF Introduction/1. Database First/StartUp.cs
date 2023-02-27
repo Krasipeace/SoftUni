99274fe3dc00b1a2c4e86e94f4bac13a74807384
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 14:27:29 +0200

    db first

---
+++ EF Core/1. EF Introduction/1. Database First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine("connection success");
+    }
+}
