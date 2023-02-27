905b282729b9a4ecad66e8cc2ee26cd4965df1ea
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 16:21:22 +0200

    task 5 completed

---
+++ EF Core/1. EF Introduction/5. Adding new address and updating employee/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string result = AddNewAddressToEmployee(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+            //context.Addresses.Add(newAddress); //Flag to Add the new address to DB
+
+            Employee? employee = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+            employee.Address = newAddress;
+
+            context.SaveChanges(); //adding the new address to DB
+
+            string[] employeeAddresses = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address!.AddressText)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, employeeAddresses);
+        }
+    }
+}
