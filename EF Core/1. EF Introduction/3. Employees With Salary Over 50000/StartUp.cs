ae37243cd39150efc06a95716932cdddb658a02c
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 15:32:04 +0200

    task 3 completed

---
+++ EF Core/1. EF Introduction/3. Employees With Salary Over 50000/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string result = GetEmployeesWithSalaryOver50000(dbContext);
+
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var employees = context.Employees
+            .OrderBy(e => e.FirstName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .Where(e => e.Salary > 50000)
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
