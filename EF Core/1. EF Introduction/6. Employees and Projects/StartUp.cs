bdaf2a06734f3239b19a492824abf2a68a69ca77
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 17:07:48 +0200

    task 6 completed

---
+++ EF Core/1. EF Introduction/6. Employees and Projects/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string result = GetEmployeesInPeriod(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            var employeesWithProjects = context.Employees
+                //.Where(e => e.EmployeesProjects
+                //    .Any(ep => ep.Project.StartDate.Year >= 2001 &&
+                //    ep.Project.StartDate.Year <= 2003))
+                .Take(10)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    ManagerFirstName = e.Manager!.FirstName,
+                    ManagerLastName = e.Manager!.LastName,
+                    Projects = e.EmployeesProjects
+                        .Where(ep => ep.Project.StartDate.Year >= 2001 &&
+                                     ep.Project.StartDate.Year <= 2003)
+                        .Select(ep => new
+                        {
+                            ProjectName = ep.Project.Name,
+                            StartDate = ep.Project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                            EndDate = ep.Project.EndDate.HasValue
+                            ? ep.Project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)
+                            : "not finished"
+                        })
+                        .ToArray()
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in employeesWithProjects)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.ManagerFirstName} {e.ManagerLastName}");
+
+                foreach (var p in e.Projects)
+                {
+                    sb.AppendLine($"--{p.ProjectName} - {p.StartDate} - {p.EndDate}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
