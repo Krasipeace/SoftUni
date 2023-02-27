ab8cf8a1a996e91cd13599a1ad1d3d98d8400543
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-27 16:04:30 +0200

    task 4 completed

---
+++ EF Core/1. EF Introduction/4. Employees from RnD/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string result = GetEmployeesFromResearchAndDevelopment(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employeesRnD = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in employeesRnD)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
