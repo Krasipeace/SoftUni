0bc6d530eb208ce975647345f9d0d3d79b21f21f
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-28 15:40:51 +0200

    task 13 completed

---
+++ EF Core/1. EF Introduction/13. Delete Project By Id/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = DeleteProjectById(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            IQueryable<EmployeeProject> epToDelete = context.EmployeesProjects
+                .Where(ep => ep.ProjectId == 2);
+
+            if (epToDelete != null)
+            {
+                context.EmployeesProjects.RemoveRange(epToDelete);
+            }
+
+            Project projectToDelete = context.Projects.Find(2)!;
+            context.Projects.Remove(projectToDelete);
+            context.SaveChanges();
+
+            string[] projectNames = context.Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, projectNames);
+        }
+    }
+}
