fac0fb23b9e9a2eee7648694fa50fa9bb2384ce1
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-28 13:52:20 +0200

    task 10 completed

---
+++ EF Core/1. EF Introduction/10. Find latest 10 projects/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Text;
+
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetLatestProjects(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var latestProjects = context.Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Description,
+                    p.StartDate
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var p in latestProjects)
+            {
+                sb.AppendLine(p.Name);
+                sb.AppendLine(p.Description);
+                sb.AppendLine(p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
