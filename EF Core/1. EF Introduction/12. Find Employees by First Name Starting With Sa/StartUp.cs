5c3659f5d090c63df7b726682b817327725e46c3
Krasimir Dramaliev <krasipeace@abv.bg>
2023-02-28 15:29:44 +0200

    task 12 completed

---
+++ EF Core/1. EF Introduction/12. Find Employees by First Name Starting With Sa/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesByFirstNameStartingWithSa(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            string[] employeesStartingwithSA = context.Employees
+                .Where(e => e.FirstName.Substring(0, 2).ToLower() == "sa")
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .Select(e => $"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:f2})")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, employeesStartingwithSA);
+        }
+    }
+}
