20c851387ccf70518724b9d495e79be3f0eb4dba
Krasimir Dramaliev <krasipeace@abv.bg>
2023-03-04 23:43:52 +0200

    adv queries: Golden Books (exercise)

--- EF Core/4. Advanced Querying/BookShop/StartUp.cs
+++ EF Core/4. Advanced Querying/BookShop/StartUp.cs
@@ -12,12 +12,13 @@ public class StartUp
         using var db = new BookShopContext();
         //DbInitializer.ResetDatabase(db);
 
-        // Age Restriction
-        string command = Console.ReadLine().ToLower();
-        string result = GetBooksByAgeRestriction(db, command);
-        Console.WriteLine(result);
+            // Age Restriction
+        //string command = Console.ReadLine().ToLower();
+        //string result = GetBooksByAgeRestriction(db, command);
+        //Console.WriteLine(result);
 
-
+            // Golden Books
+        Console.WriteLine(GetGoldenBooks(db));
     }
 
     // Age Restriction
@@ -40,6 +41,26 @@ public class StartUp
 
         return sb.ToString().TrimEnd();
     }
+
+    // Golden Books
+    public static string GetGoldenBooks(BookShopContext context)
+    {
+        var goldenBooks = context.Books
+            .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+            .OrderBy(b => b.BookId)
+            .Select(b => b.Title)
+            .ToArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var book in goldenBooks)
+        {
+            sb.AppendLine(book);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
 }
 
 
