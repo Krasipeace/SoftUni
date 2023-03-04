7019e80f6666c014378867c176a9823042c9e33d
Krasimir Dramaliev <krasipeace@abv.bg>
2023-03-04 23:26:01 +0200

    adv queries: age restriction (exercise)

---
+++ EF Core/4. Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data;
+
+internal class Configuration
+{
+    internal static string ConnectionString
+        => @"Server=DESKTOP-0OUMPBE; Database=BookShop; Integrated Security = True;TrustServerCertificate=True;";
+}
---
+++ EF Core/4. Advanced Querying/BookShop/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop;
+
+using BookShop.Models.Enums;
+using Data;
+using Initializer;
+using System.Text;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var db = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+
+        // Age Restriction
+        string command = Console.ReadLine().ToLower();
+        string result = GetBooksByAgeRestriction(db, command);
+        Console.WriteLine(result);
+
+
+    }
+
+    // Age Restriction
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        var ageCategories = Enum.Parse<AgeRestriction>(command, true);
+
+        var bookTitles = context.Books
+            .Where(b => b.AgeRestriction == ageCategories)
+            .Select(b => b.Title)
+            .OrderBy(title => title)
+            .ToArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var book in bookTitles)
+        {
+            sb.AppendLine(book);
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
+
+
