ae514fe7b5d7d8ca21106c1a6486100c66a11715
Krasimir Dramaliev <krasipeace@abv.bg>
2023-03-20 15:49:58 +0200

    import with dto

--- EF Core/5. JSON/CarDealer/StartUp.cs
+++ EF Core/5. JSON/CarDealer/StartUp.cs
@@ -2,6 +2,7 @@
 
 using CarDealer.Data;
 using CarDealer.Models;
+using CarDealer.DTOs.Import;
 
 namespace CarDealer;
 
@@ -15,19 +16,26 @@ public class StartUp
         //context.Database.EnsureCreated();
         //Console.WriteLine("Database CarDealer created...");
 
-        // Import Suppliers 
+        //// Import Suppliers 
         //string inputSuppliersFromFile = @"../../../Datasets/suppliers.json";
         //string readSuppliersFromFile = File.ReadAllText(inputSuppliersFromFile);
         //string importSuppliersToDB = ImportSuppliers(context, readSuppliersFromFile);
         //Console.WriteLine(importSuppliersToDB);
 
-        // Import Parts
-        string inputPartsFromFIle = @"../../../Datasets/parts.json";
-        string readPartsFromFile = File.ReadAllText(inputPartsFromFIle);
-        string importPartsToDB = ImportParts(context, readPartsFromFile);
-        Console.WriteLine(importPartsToDB);
+        //// Import Parts
+        //string inputPartsFromFIle = @"../../../Datasets/parts.json";
+        //string readPartsFromFile = File.ReadAllText(inputPartsFromFIle);
+        //string importPartsToDB = ImportParts(context, readPartsFromFile);
+        //Console.WriteLine(importPartsToDB);
+
+        // Import Cars
+        string inputCarsFromFile = @"../../../Datasets/cars.json";
+        string readCarsFromFile = File.ReadAllText(inputCarsFromFile);
+        string importCarsToDB = ImportCars(context, readCarsFromFile);
+        Console.WriteLine(importCarsToDB);
     }
 
+    // Import Data
     public static string ImportSuppliers(CarDealerContext context, string inputJson)
     {
         var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
@@ -51,5 +59,39 @@ public class StartUp
         context.SaveChanges();
 
         return $"Successfully imported {parts.Count}.";
+    }
+
+    public static string ImportCars(CarDealerContext context, string inputJson)
+    {
+        var carsWithParts = JsonConvert.DeserializeObject<List<ImportCarsDTO>>(inputJson);
+        var parts = new List<PartCar>();
+        var cars = new List<Car>();
+
+        foreach (var car in carsWithParts)
+        {
+            Car importCar = new Car()
+            {
+                Make = car.Make,
+                Model = car.Model,
+                TravelledDistance = car.TravelledDistance
+            };
+            cars.Add(importCar);
+
+            foreach (var part in car.PartsId.Distinct())
+            {
+                PartCar importPart = new PartCar()
+                {
+                    Car = importCar,
+                    PartId = part
+                };
+                parts.Add(importPart);
+            }
+        }
+
+        context.Cars.AddRange(cars);
+        context.PartsCars.AddRange(parts);
+        context.SaveChanges();
+
+        return $"Successfully imported {cars.Count}.";
     }
 }
