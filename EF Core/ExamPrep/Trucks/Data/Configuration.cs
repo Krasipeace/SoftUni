2641c553734f6b73666ae24d83bbe3281f6ea395
Krasimir Dramaliev <krasipeace@abv.bg>
2023-03-28 19:48:22 +0300

    exam prep (15 august 2022) -> task 1: model definition

---
+++ EF Core/ExamPrep/Trucks/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Trucks.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-0OUMPBE;Database=Trucks;Integrated Security=True;TrustServerCertificate=True;";
+    }
+}
---
+++ EF Core/ExamPrep/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,43 @@
+﻿namespace Trucks.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using Trucks.Data.Models.Enums;
+
+    public class Truck
+    {
+        public Truck () 
+        { 
+            this.ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        [MaxLength(17)]
+        public string VinNumber { get; set; }
+
+        public int TankCapacity { get; set; }
+
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        [Required]
+        public MakeType MakeType { get; set; }
+
+        [ForeignKey(nameof(Despatcher))]
+        public int DespatcherId { get; set; }
+
+        public virtual Despatcher Despatcher { get; set; }
+
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
