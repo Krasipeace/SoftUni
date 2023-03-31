d7644215fa722344f94269f90f76db6a261cd53a
Krasimir Dramaliev <krasipeace@abv.bg>
2023-03-31 19:03:24 +0300

    exam prep -> import/export

---
+++ EF Core/ExamPrep/Trucks/DataProcessor/ImportDto/ImportTruckDto.cs
@@ -0,0 +1,37 @@
+﻿namespace Trucks.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Truck")]
+    public class ImportTruckDto
+    {
+        [XmlElement("RegistrationNumber")]
+        [MinLength(8)]
+        [MaxLength(8)]
+        [RegularExpression(@"[A-Z]{2}\d{4}[A-Z]{2}")]      
+        public string? RegistrationNumber { get; set; }
+
+        [XmlElement("VinNumber")]
+        [Required]
+        [MinLength(17)]
+        [MaxLength(17)]
+        public string VinNumber { get; set; } = null!;
+
+        [XmlElement("TankCapacity")]
+        [Range(950, 1420)]
+        public int TankCapacity { get; set; }
+
+        [XmlElement("CargoCapacity")]
+        [Range(5000, 29000)]
+        public int CargoCapacity { get; set; }
+
+        [XmlElement("CategoryType")]
+        [Range(0, 3)]
+        public int CategoryType { get; set; }
+
+        [XmlElement("MakeType")]
+        [Range(0, 4)]
+        public int MakeType { get; set; }
+    }
+}
--- EF Core/ExamPrep/Trucks/DataProcessor/Serializer.cs
+++ EF Core/ExamPrep/Trucks/DataProcessor/Serializer.cs
@@ -1,17 +1,78 @@
 ﻿namespace Trucks.DataProcessor
 {
     using Data;
+    using Microsoft.EntityFrameworkCore;
+
+    using Newtonsoft.Json;
+
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ExportDto;
+    using Trucks.Utilities;
 
     public class Serializer
     {
+        private static XmlHelper xmlHelper;
+
         public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
         {
-            throw new NotImplementedException();
+            xmlHelper = new XmlHelper();
+
+            ExportDespatcherDto[] despatchers = context
+                .Despatchers
+                .Where(d => d.Trucks.Any())
+                .ToArray()
+                .Select(d => new ExportDespatcherDto()
+                {
+                    TrucksCount = d.Trucks.Count,
+                    DespatcherName = d.Name,
+                    Trucks = d.Trucks
+                        .Select(t => new ExportTruckDto()
+                        {
+                            RegistrationNumber = t.RegistrationNumber,
+                            Make = t.MakeType.ToString()
+                        })
+                        .OrderBy(t => t.RegistrationNumber)
+                        .ToArray()
+                })
+                .OrderByDescending(d => d.TrucksCount)
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            return xmlHelper.Serialize(despatchers, "Despatchers");
         }
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
         {
-            throw new NotImplementedException();
+            var clients = context.Clients
+                .Include(c => c.ClientsTrucks)
+                .ThenInclude(ct => ct.Truck)
+                .AsNoTracking()
+                .ToArray()
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                .Select(c => new
+                {
+                    c.Name,
+                    Trucks = c.ClientsTrucks
+                        .Where(ct => ct.Truck.TankCapacity >= capacity)
+                        .Select(ct => new
+                        {
+                            TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                            VinNumber = ct.Truck.VinNumber,
+                            TankCapacity = ct.Truck.TankCapacity,
+                            CargoCapacity = ct.Truck.CargoCapacity,
+                            CategoryType = ct.Truck.CategoryType.ToString(),
+                            MakeType = ct.Truck.MakeType.ToString()
+                        })
+                        .OrderBy(t => t.MakeType)
+                        .ThenByDescending(t => t.CargoCapacity)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Length)
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);
         }
     }
 }
