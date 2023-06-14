498260023664132390f1d792c294fc823a291f8c
Krasimir Dramaliev <krasipeace@abv.bg>
2023-06-14 17:12:45 +0300

    exam prep -> migration ->custom tables added

---
+++ ASP.NET Fundamentals/ExamPrep/Library/Data/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data.Models
+{
+    [Comment("Categories for the library")]
+    public class Category
+    {
+        [Comment("Primary key for the category")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Comment("Books in the category")]
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ ASP.NET Fundamentals/ExamPrep/Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data.Models
+{
+    [Comment("User books")]
+    public class IdentityUserBook
+    {
+        [Comment("Id of the collector")]
+        public string CollectorId { get; set; } = null!;
+
+        [Comment("Collector")]
+        [ForeignKey(nameof(CollectorId))]
+        public IdentityUser Collector { get; set; } = null!;
+
+        [Comment("Id of the book")]
+        public int BookId { get; set; }
+
+        [Comment("Book")]
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+    }
+}
