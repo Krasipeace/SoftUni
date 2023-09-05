afe26d48839b5fb23f52cdacaa1eb8860647a659
Krasimir Dramaliev <krasipeace@abv.bg>
2023-09-05 18:30:05 +0300

    solved `reverse numbers with a stack`

---
+++ Data Structures - Fundamentals/02. Linear Data Structures - Exercise/05.ReverseNumbersWithaStack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.ReverseNumbersWithaStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write numbers(1 2 3 4 5 etc.):");
+            int[] input = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            Stack<int> stack = new();
+            foreach (int num in input)
+            {
+                stack.Push(num);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+        }
+    }
+}
