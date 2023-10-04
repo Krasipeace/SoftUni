ac51680e200ae42d8f3da9d100109af48ebce08b
Krasimir Dramaliev <krasipeace@abv.bg>
2023-10-04 16:49:17 +0300

    exercising `data structures`

---
+++ Data Structures - Fundamentals/ExamPrep/Exam26032022/BarberShop/BarberShop.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BarberShop
+{
+    public class BarberShop : IBarberShop
+    {
+        private readonly Dictionary<string, Barber> barbers = new Dictionary<string, Barber>();
+        private readonly Dictionary<string, Client> clients = new Dictionary<string, Client>();
+        private readonly Dictionary<string, List<string>> barberWithClients = new Dictionary<string, List<string>>();
+
+        public void AddBarber(Barber b)
+        {
+            if (this.Exist(b))
+            {
+                throw new ArgumentException("Barber already exists");
+            }
+
+            this.barbers.Add(b.Name, b);
+            this.barberWithClients.Add(b.Name, new List<string>());
+        }
+
+        public void AddClient(Client c)
+        {
+            if (this.Exist(c))
+            {
+                throw new ArgumentException("Client already exists");
+            }
+
+            this.clients.Add(c.Name, c);
+        }
+
+        public bool Exist(Barber b)
+            => this.barbers.ContainsKey(b.Name);
+
+        public bool Exist(Client c)
+            => this.clients.ContainsKey(c.Name);
+
+        public IEnumerable<Barber> GetBarbers()
+            => this.barbers.Values.ToArray();
+
+        public IEnumerable<Client> GetClients()
+            => this.clients.Values.ToArray();
+
+        public void AssignClient(Barber b, Client c)
+        {
+            if (!this.Exist(b) || !this.Exist(c))
+            {
+                throw new ArgumentException();
+            }
+
+            c.Barber = b;
+            this.barberWithClients[b.Name].Add(c.Name);
+        }
+
+        public void DeleteAllClientsFrom(Barber b)
+        {
+            if (!this.Exist(b))
+            {
+                throw new ArgumentException();
+            }
+
+            foreach (var client in clients.Where(c => c.Value.Barber.Name == b.Name))
+            {
+                this.clients.Remove(client.Key);
+            }
+        }
+
+        public IEnumerable<Client> GetClientsWithNoBarber()
+            => this.clients.Values
+            .Where(c => c.Barber == null)
+            .ToArray();
+
+        public IEnumerable<Barber> GetAllBarbersSortedWithClientsCountDesc()       
+            => this.barberWithClients
+                .OrderByDescending(b => b.Value.Count)
+                .Select(b => this.barbers[b.Key]);
+
+        public IEnumerable<Barber> GetAllBarbersSortedWithStarsDecsendingAndHaircutPriceAsc()
+            => this.barbers.Values
+                .OrderByDescending(b => b.Stars)
+                .ThenBy(b => b.HaircutPrice);
+
+        public IEnumerable<Client> GetClientsSortedByAgeDescAndBarbersStarsDesc()
+            => this.clients.Values
+                .OrderByDescending(c => c.Age)
+                .ThenByDescending(c => c.Barber.Stars);
+    }
+}
---
+++ Data Structures - Fundamentals/ExamPrep/Exam26032022/TripAdministrator/TripAdministrator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TripAdministrations
+{
+    public class TripAdministrator : ITripAdministrator
+    {
+        private readonly Dictionary<string, Company> companies = new Dictionary<string, Company>();
+        private readonly Dictionary<string, Trip> trips = new Dictionary<string, Trip>();
+        private readonly Dictionary<Company, List<Trip>> companyWithTrips = new Dictionary<Company, List<Trip>>();
+
+        public void AddCompany(Company c)
+        {
+            if (this.Exist(c))
+            {
+                throw new ArgumentException();
+            }
+            
+            this.companies.Add(c.Name, c);
+            this.companyWithTrips.Add(c, new List<Trip>());
+        }
+
+        public void AddTrip(Company c, Trip t)
+        {
+            if (!this.Exist(c))
+            {
+                throw new ArgumentException();
+            }
+
+            this.trips.Add(t.Id, t);
+            this.companyWithTrips[c].Add(t);
+        }
+
+        public bool Exist(Company c)
+            => this.companies.ContainsKey(c.Name);
+
+        public bool Exist(Trip t)
+            => this.trips.ContainsKey(t.Id);
+
+        public void RemoveCompany(Company c)
+        {
+            if (!this.Exist(c))
+            {
+                throw new ArgumentException();
+            }
+
+            var trips = this.companyWithTrips[c];
+            foreach (var trip in trips)
+            {
+                this.trips.Remove(trip.Id);
+            }
+
+            this.companyWithTrips.Remove(c);
+            this.companies.Remove(c.Name);
+        }
+
+        public IEnumerable<Company> GetCompanies()
+            => this.companies.Values;
+
+        public IEnumerable<Trip> GetTrips()
+            => this.trips.Values;
+
+        public void ExecuteTrip(Company c, Trip t)
+        {
+            if (!this.Exist(c) 
+                || !this.Exist(t)
+                || !this.companyWithTrips[c].Contains(t))
+            {
+                throw new ArgumentException();
+            }
+            
+            this.trips.Remove(t.Id);
+            this.companyWithTrips[c].Remove(t);
+        }
+
+        public IEnumerable<Company> GetCompaniesWithMoreThatNTrips(int n)
+            => this.companyWithTrips
+            .Where(c => c.Value.Count > n)
+            .Select(c => c.Key);
+            
+        public IEnumerable<Trip> GetTripsWithTransportationType(Transportation t)
+            => this.trips.Values
+                .Where(trip => trip.Transportation == t);
+
+        public IEnumerable<Trip> GetAllTripsInPriceRange(int lo, int hi)
+            => this.trips.Values
+                .Where(t => t.Price >= lo && t.Price <= hi);
+    }
+}
