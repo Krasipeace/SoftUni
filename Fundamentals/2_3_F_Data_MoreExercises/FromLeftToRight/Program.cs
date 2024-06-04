fc88b90f3259f6bc9e7a173bbc354952a1aef4ae
Krasimir Dramaliev <krasipeace@abv.bg>
2024-06-04 12:46:23 +0300

    Update Program.cs

--- Fundamentals/2_3_F_Data_MoreExercises/FromLeftToRight/Program.cs
+++ Fundamentals/2_3_F_Data_MoreExercises/FromLeftToRight/Program.cs
@@ -1,46 +1,38 @@
-﻿using System;
+namespace Demo;
 
-namespace FromLeftToRight
+public class CompareAndSumDigits
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int input = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < input; i++)
         {
-            int input = int.Parse(Console.ReadLine());
+            long greaterLeft = 0;
+            long greaterRight = 0;
 
-            for (int i = 0; i < input; i++)
-            {
-                long greaterLeft = 0;
-                long greaterRight = 0;
+            string[] numbers = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string left = numbers[0];
+            string right = numbers[1];
 
-                string[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                string left = numbers[0];
-                string right = numbers[1];
+            long numberLeft = long.Parse(left);
+            long numberRight = long.Parse(right);
 
-                long numberLeft = long.Parse(left);
-                long numberRight = long.Parse(right);
+            greaterLeft = Calculate(greaterLeft, left);
+            greaterRight = Calculate(greaterRight, right);
 
-                for (int l = 0; l < left.Length; l++)
-                {
-                    bool isDigit = long.TryParse(left[l].ToString(), out long digit);
-                    greaterLeft += digit;
-                }
-                for (int j = 0; j < right.Length; j++)
-                {
-                    bool isDigit = long.TryParse(right[j].ToString(), out long digit);
-                    greaterRight += digit;
-                }
-
-                if (numberLeft > numberRight)
-                {
-                    Console.WriteLine(greaterLeft);
-                }
-                else
-                {
-                    Console.WriteLine(greaterRight);
-                }
-
-            }
+            Console.WriteLine(numberLeft > numberRight ? greaterLeft : greaterRight);
         }
+    }
+
+    private static long Calculate(long greaterSide, string side)
+    {
+        for (int l = 0; l < side.Length; l++)
+        {
+            bool isDigit = long.TryParse(side[l].ToString(), out long digit);
+            greaterSide += digit;
+        }
+
+        return greaterSide;
     }
 }
