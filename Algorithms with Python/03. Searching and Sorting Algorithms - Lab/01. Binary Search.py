d8d389ca835f1c0dea8435ca1309b9fab74adce4
Krasimir Dramaliev <krasipeace@abv.bg>
2024-11-12 13:51:28 +0200

    added recursive_binary_search and tested execution times

--- Algorithms with Python/03. Searching and Sorting Algorithms - Lab/01. Binary Search.py
+++ Algorithms with Python/03. Searching and Sorting Algorithms - Lab/01. Binary Search.py
@@ -1,3 +1,4 @@
+# import time
 def binary_search(numbers, target):
     left = 0
     right = len(numbers) - 1
@@ -16,6 +17,30 @@ def binary_search(numbers, target):
 
     return -1
 
+def binary_search_recursive(numbers, target, left, right):
+    if left > right:
+        return -1
+
+    mid = (left + right) // 2
+    mid_element = numbers[mid]
+
+    if mid_element == target:
+        return mid
+    elif mid_element < target:
+        return binary_search_recursive(numbers, target, mid + 1, right)
+    else:
+        return binary_search_recursive(numbers, target, left, mid - 1)
+
 numbers = [int(n) for n in input().split()]
+# numbers2 = numbers
 target = int(input())
+
+# start_time = time.time()
 print(binary_search(numbers, target))
+# end_time = time.time()
+# print(f"Execution time: {end_time - start_time:.6f} seconds.")
+
+# start_time = time.time()
+# print(binary_search_recursive(numbers2, target, 0, len(numbers2) - 1))
+# end_time = time.time()
+# print(f"Execution time Recursively: {end_time - start_time:.6f} seconds.")
